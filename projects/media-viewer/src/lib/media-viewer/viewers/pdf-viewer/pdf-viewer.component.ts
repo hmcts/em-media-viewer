3115fda554e332ec9a53bcca8b30e481505b17f3
pawelkaczmarekuk <pawel.kaczmarek@gmail.com>
2019-04-25 13:02:14 +0100

    cleanup

--- projects/media-viewer/src/lib/media-viewer/viewers/pdf-viewer/pdf-viewer.component.ts
+++ projects/media-viewer/src/lib/media-viewer/viewers/pdf-viewer/pdf-viewer.component.ts
@@ -1,25 +1,4 @@
-import {
-  Component,
-  OnInit,
-  ViewChild,
-  ElementRef,
-  Input,
-  ChangeDetectorRef,
-  Renderer2,
-  OnDestroy,
-  ComponentRef, AfterViewInit
-} from '@angular/core';
-import { Subscription } from 'rxjs';
-import {PdfService} from '../../../data/pdf.service';
-import { Utils } from '../../../data/utils';
-import { PdfAnnotateWrapper } from '../../../data/js-wrapper/pdf-annotate-wrapper';
-import { CommentsComponent } from '../../annotation/comments/comments.component';
-import { ContextualToolbarComponent } from './contextual-toolbar/contextual-toolbar.component';
-import { RenderOptions } from '../../../data/js-wrapper/renderOptions.model';
-import { PdfRenderService } from '../../../data/pdf-render.service';
-import { RotationFactoryService } from './rotation-toolbar/rotation-factory.service';
-import { RotationComponent } from './rotation-toolbar/rotation.component';
-import { RotationModel } from '../../../model/rotation-factory.model';
+import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {MediaViewerFeatures} from '../../media-viewer-features';
 import {PdfWrapper} from '../../../data/js-wrapper/pdf-wrapper';
 
@@ -31,14 +10,15 @@ import {PdfWrapper} from '../../../data/js-wrapper/pdf-wrapper';
 })
 export class PdfViewerComponent implements AfterViewInit {
 
+  @Input() url: string;
+  @Input() mediaViewerFeatures = new MediaViewerFeatures();
+  @ViewChild('viewerContainer') viewerContainer: ElementRef;
+
   constructor(private pdfWrapper: PdfWrapper) {
   }
 
   ngAfterViewInit(): void {
-      this.pdfWrapper.initViewer(this.url);
+      this.pdfWrapper.initViewer(this.url, this.viewerContainer);
   }
-
-  @Input() url: string;
-  @Input() mediaViewerFeatures = new MediaViewerFeatures();
 
 }
