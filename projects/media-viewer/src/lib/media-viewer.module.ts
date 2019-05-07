9da59507b206f16495c130ba973bc4a6c9fa03a8
benouaer <tech@benouaer.com>
2019-05-07 18:02:27 +0100

    remove references to annotations

--- projects/media-viewer/src/lib/media-viewer.module.ts
+++ projects/media-viewer/src/lib/media-viewer.module.ts
@@ -2,18 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { CommentsComponent } from './media-viewer/annotation/comments/comments.component';
-import { CommentItemComponent } from './media-viewer/annotation/comments/comment-item/comment-item.component';
 import { PdfViewerComponent } from './media-viewer/viewers/pdf-viewer/pdf-viewer.component';
-import { AnnotationApiHttpService } from './media-viewer/annotation/annotation-api-http.service';
-import { ContextualToolbarComponent } from './media-viewer/viewers/pdf-viewer/contextual-toolbar/contextual-toolbar.component';
 import { PdfJsWrapper } from './media-viewer/viewers/pdf-viewer/pdf-js/pdf-js-wrapper';
 import { NgtUniversalModule } from '@ng-toolkit/universal';
 import { ImageViewerComponent } from './media-viewer/viewers/image-viewer/image-viewer.component';
 import { UnsupportedViewerComponent } from './media-viewer/viewers/unsupported-viewer/unsupported-viewer.component';
 import { MediaViewerComponent } from './media-viewer/media-viewer.component';
 import { MediaViewerService } from './media-viewer/media-viewer.service';
-import { RotationComponent } from './media-viewer/viewers/pdf-viewer/rotation-toolbar/rotation.component';
 import { EmLoggerService } from './logging/em-logger.service';
 import {MediaViewerMessageService} from './media-viewer/service/media-viewer-message.service';
 
@@ -25,24 +20,18 @@ import {MediaViewerMessageService} from './media-viewer/service/media-viewer-mes
     HttpClientModule
   ],
   declarations: [
-    CommentsComponent,
-    CommentItemComponent,
-    ContextualToolbarComponent,
     PdfViewerComponent,
     ImageViewerComponent,
     UnsupportedViewerComponent,
     MediaViewerComponent,
-    RotationComponent
   ],
   entryComponents: [
     PdfViewerComponent,
     ImageViewerComponent,
     UnsupportedViewerComponent,
-    RotationComponent
 ],
   providers: [
     PdfJsWrapper,
-    AnnotationApiHttpService,
     MediaViewerService,
     EmLoggerService,
     MediaViewerMessageService
--- projects/media-viewer/src/lib/media-viewer/viewers/pdf-viewer/pdf-viewer.component.spec.ts
+++ projects/media-viewer/src/lib/media-viewer/viewers/pdf-viewer/pdf-viewer.component.spec.ts
@@ -1,80 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { Subject } from 'rxjs';
 
 import { PdfViewerComponent } from './pdf-viewer.component';
 
-import { AnnotationStoreService } from '../../annotation/annotation-store.service';
-import { AnnotationApiHttpService } from '../../annotation/annotation-api-http.service';
-import { CommentsComponent } from '../../annotation/comments/comments.component';
 import { EmLoggerService } from '../../../logging/em-logger.service';
-
-class MockPdfService {
-  pageNumber: Subject<number>;
-
-  preRun() {
-    this.pageNumber = new Subject();
-    this.pageNumber.next(1);
-  }
-
-}
-
-class MockAnnotationStoreService {
-  preLoad() {}
-  setCommentBtnSubject(commentId: string) {}
-  setAnnotationFocusSubject() {}
-  getAnnotationFocusSubject() {}
-}
-
-
-
-class MockApiHttpService {
-  getBaseUrl() {}
-  setBaseUrl(baseUrl) {}
-}
-
-class MockUtils {
-  clickIsHighlight() {}
-  getClickedPage() {}
-}
-
-class MockViewerComponent {
-  nativeElement: { querySelector() };
-}
-
-class MockPdfRenderService {
-
-  listPagesSubject = new Subject();
-
-  setRenderOptions() {}
-  getRenderOptions() {}
-  getDataLoadedSub() {}
-  render() {}
-  getPdfPages() {}
-}
-
-class MockRotationFactoryService {
-}
 
 describe('PdfViewerComponent', () => {
   let component: PdfViewerComponent;
   let fixture: ComponentFixture<PdfViewerComponent>;
 
-  const mockPdfRenderService = new MockPdfRenderService();
-  const mockAnnotationStoreService = new MockAnnotationStoreService();
-  const mockPdfService = new MockPdfService();
-  const mockApiHttpService = new MockApiHttpService();
-  const mockUtils = new MockUtils();
-  const mockViewerComponent = new MockViewerComponent();
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PdfViewerComponent, CommentsComponent ],
+      declarations: [ PdfViewerComponent ],
       schemas: [NO_ERRORS_SCHEMA],
       providers: [
-        EmLoggerService,
-        { provide: AnnotationStoreService, useFactory: () => mockAnnotationStoreService },
-        { provide: AnnotationApiHttpService, useFactory: () => mockApiHttpService },
+        EmLoggerService
       ]
     })
     .compileComponents();
