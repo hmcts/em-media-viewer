6be99cb4a059fd6c28371334a2f16f4a47a29397
Linus Norton <linusnorton@gmail.com>
2019-05-08 13:01:13 +0100

    Toolbar (#6)
    * Added toolbar component
    
    * tidy up

---
+++ projects/media-viewer/src/lib/toolbar/toolbar.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToolbarComponent } from './toolbar.component';
+
+@NgModule({
+  declarations: [ToolbarComponent],
+  exports: [
+    ToolbarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ToolbarModule { }
