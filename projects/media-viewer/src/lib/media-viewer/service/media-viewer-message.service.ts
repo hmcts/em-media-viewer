8e02dcb0c6bd41c7a5c998c17ff45946c17bc8c1
benouaer <tech@benouaer.com>
2019-05-08 17:09:14 +0100

    consolidate external properties

--- projects/media-viewer/src/lib/media-viewer/service/media-viewer-message.service.ts
+++ projects/media-viewer/src/lib/media-viewer/service/media-viewer-message.service.ts
@@ -1,38 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Subject } from 'rxjs/Subject';
-import { RotateOperation, SearchOperation, ZoomOperation } from './media-viewer-message.model';
+import {
+  RotateOperation,
+  SearchOperation,
+  ZoomOperation
+} from './media-viewer-message.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MediaViewerMessageService {
 
-  private rotate = new Subject<RotateOperation>();
-  private search = new Subject<SearchOperation>();
-  private zoom = new Subject<ZoomOperation>();
-
-  triggerRotation(message: RotateOperation) {
-    this.rotate.next(message);
-  }
-
-  triggerSearch(message: SearchOperation) {
-    this.search.next(message);
-  }
-
-  triggerZoom(message: ZoomOperation) {
-    this.zoom.next(message);
-  }
-
-  rotationEvent(): Observable<RotateOperation> {
-    return this.rotate.asObservable();
-  }
-
-  searchEvent(): Observable<SearchOperation> {
-    return this.search.asObservable();
-  }
-
-  zoomEvent(): Observable<ZoomOperation> {
-    return this.zoom.asObservable();
+  actionEvents() {
+    return {
+      rotate: new Subject<RotateOperation>(),
+      search: new Subject<SearchOperation>(),
+      zoom: new Subject<ZoomOperation>()
+    };
   }
 }
