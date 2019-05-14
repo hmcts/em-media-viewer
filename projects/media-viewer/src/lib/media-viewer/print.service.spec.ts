1027f756e6a77fcc0cc3f276a01120d7a7bb9667
pawelkaczmarekuk <pawel.kaczmarek@gmail.com>
2019-05-14 16:24:38 +0200

    download and print

---
+++ projects/media-viewer/src/lib/media-viewer/print.service.spec.ts
@@ -0,0 +1,34 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { MediaViewerService } from './media-viewer.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TransferState } from '@angular/platform-browser';
+import {EmLoggerService} from '../logging/em-logger.service';
+import {PrintService} from './print.service';
+
+describe('PrintService', () => {
+    let httpMock: HttpTestingController;
+
+    beforeEach(() => {
+
+        TestBed.configureTestingModule({
+          providers: [
+              EmLoggerService,
+              PrintService,
+              TransferState,
+              EmLoggerService
+            ],
+            imports: [HttpClientTestingModule]
+        });
+
+        httpMock = TestBed.get(HttpTestingController);
+
+      });
+
+    describe('constructor', () => {
+        it('should be created', inject([MediaViewerService], (service: MediaViewerService) => {
+          expect(service).toBeTruthy();
+        }));
+    });
+
+});
+
---
+++ projects/media-viewer/src/lib/media-viewer/print.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrintService {
+
+  printDocumentNatively(url) {
+    const printWindow = window.open(url);
+    printWindow.print();
+  }
+
+}
