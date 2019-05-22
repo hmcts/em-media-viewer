7693806835ffb24dbf924515ff72cfc7ab09de34
Linus Norton <linusnorton@gmail.com>
2019-05-22 17:05:09 +0100

    do what taleb tells me to

--- projects/media-viewer/src/lib/media-viewer/viewers/pdf-viewer/pdf-viewer.component.spec.ts
+++ projects/media-viewer/src/lib/media-viewer/viewers/pdf-viewer/pdf-viewer.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { Subject } from 'rxjs';
 import { PdfViewerComponent } from './pdf-viewer.component';
 import { EmLoggerService } from '../../../logging/em-logger.service';
@@ -42,7 +41,6 @@ describe('PdfViewerComponent', () => {
 
     return TestBed.configureTestingModule({
       declarations: [ PdfViewerComponent ],
-      schemas: [NO_ERRORS_SCHEMA],
       providers: [
         EmLoggerService,
         { provide: PdfJsWrapperFactory, useValue: mockFactory }
