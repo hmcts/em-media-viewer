3cc671a210b1d35ad20d8f1e9db25871f74875ff
Linus Norton <linusnorton@gmail.com>
2019-05-24 10:32:10 +0100

    Moved tests (#41)
    Moved tests to wrapper and added print service test

--- projects/media-viewer/src/lib/media-viewer/viewers/pdf-viewer/pdf-js/pdf-js-wrapper.spec.ts
+++ projects/media-viewer/src/lib/media-viewer/viewers/pdf-viewer/pdf-js/pdf-js-wrapper.spec.ts
@@ -1,31 +1,58 @@
 import { PdfJsWrapper } from './pdf-js-wrapper';
-import { SearchResultsCount, SetCurrentPageOperation } from '../../../model/viewer-operations';
+import {
+  ChangePageByDeltaOperation,
+  RotateOperation, SearchOperation,
+  SearchResultsCount,
+  SetCurrentPageOperation,
+  StepZoomOperation,
+  ZoomOperation
+} from '../../../model/viewer-operations';
 import { Subject } from 'rxjs';
 import * as pdfjsViewer from 'pdfjs-dist/web/pdf_viewer';
 import * as pdfjsLib from 'pdfjs-dist';
+import { ToolbarToggles } from '../../../model/toolbar-toggles';
+import { PdfViewerComponent } from '../pdf-viewer.component';
+import { EmLoggerService } from '../../../../logging/em-logger.service';
+import { PdfJsWrapperFactory } from './pdf-js-wrapper.provider';
 
 describe('PdfJsWrapper', () => {
+  const downloadManager = new pdfjsViewer.DownloadManager({});
 
-  let eventBus: any;
-  let eventBusSpy: any;
-  let downloadManager: any;
-  let pdfViewer: any;
-  let wrapper: PdfJsWrapper;
+  const mockViewer = {
+    pagesRotation: 0,
+    currentPageNumber: 1,
+    currentScaleValue: 2,
+    eventBus: {
+      on: () => {
+      },
+      dispatch: () => {
+      }
+    },
+    setDocument: () => {},
+    linkService: {
+      setDocument: () => {}
+    },
+    findController: {
+      executeCommand: () => {}
+    }
+  };
 
-  beforeEach(() => {
-    eventBus = { on: () => {} };
-    eventBusSpy = spyOn(eventBus, 'on');
-    downloadManager = new pdfjsViewer.DownloadManager({});
-    pdfViewer = { eventBus } as any;
-    wrapper = new PdfJsWrapper(
-      new Subject<SearchResultsCount>(),
-      new Subject<SetCurrentPageOperation>(),
-      pdfViewer,
-      downloadManager
-    );
-  });
+  const wrapper = new PdfJsWrapper(
+    new Subject<SearchResultsCount>(),
+    new Subject<SetCurrentPageOperation>(),
+    mockViewer,
+    downloadManager
+  );
 
   it('set up eventbus listeners', () => {
+    const eventBusSpy = spyOn(mockViewer.eventBus, 'on');
+    const _ = new PdfJsWrapper(
+      new Subject<SearchResultsCount>(),
+      new Subject<SetCurrentPageOperation>(),
+      mockViewer,
+      downloadManager
+    );
+
     expect(eventBusSpy).toHaveBeenCalledTimes(4);
   });
 
@@ -37,25 +64,84 @@ describe('PdfJsWrapper', () => {
   });
 
   it('loads a document', async () => {
-     eventBus = { on: () => {} };
-    const setDocument = () => {};
-    const linkService = { setDocument };
-    pdfViewer = { eventBus, linkService, setDocument } as any;
-    const pdfViewerSpy = spyOn(pdfViewer, 'setDocument');
+    const pdfViewerSpy = spyOn(mockViewer, 'setDocument');
     const mockDocument = {};
 
     // hack out the pdf.js function
     pdfjsLib.getDocument = () => Promise.resolve(mockDocument);
 
-    wrapper = new PdfJsWrapper(
-      new Subject<SearchResultsCount>(),
-      new Subject<SetCurrentPageOperation>(),
-      pdfViewer,
-      new pdfjsViewer.DownloadManager({})
-    );
-
     await wrapper.loadDocument({} as any);
 
     expect(pdfViewerSpy).toHaveBeenCalledWith(mockDocument);
+  });
+
+  it('should perform rotate operation', () => {
+    mockViewer.pagesRotation = 0;
+    wrapper.rotate(90);
+
+    expect(mockViewer.pagesRotation).toEqual(90);
+  });
+
+  it('should perform zoom operation', () => {
+    mockViewer.currentScaleValue = 1;
+    wrapper.setZoom(2);
+
+    expect(mockViewer.currentScaleValue).toEqual(2);
+  });
+
+  it('should set scale value to max value', () => {
+    mockViewer.currentScaleValue = 1;
+    wrapper.setZoom(6);
+
+    expect(mockViewer.currentScaleValue).toEqual(5);
+  });
+
+  it('should set scale value to min value', () => {
+    mockViewer.currentScaleValue = 1;
+    wrapper.setZoom(0.001);
+
+    expect(mockViewer.currentScaleValue).toEqual(0.1);
+  });
+
+  it('should step the zoom', () => {
+    mockViewer.currentScaleValue = 1;
+    wrapper.stepZoom(0.5);
+
+    expect(mockViewer.currentScaleValue).toEqual(1.5);
+  });
+
+  it('should call the search operation', () => {
+    spyOn(mockViewer.findController, 'executeCommand');
+    const searchOperation = new SearchOperation('searchTerm', false, false, false, false, false);
+
+    wrapper.search(searchOperation);
+
+    expect(mockViewer.findController.executeCommand).toHaveBeenCalledWith('findagain', {
+      query: 'searchTerm',
+      caseSensitive: false,
+      entireWord: false,
+      highlightAll: false,
+      findPrevious: false
+    });
+  });
+
+  it('clear the search when the search bar is closed', () => {
+    spyOn(mockViewer.eventBus, 'dispatch');
+
+    wrapper.clearSearch();
+
+    expect(mockViewer.eventBus.dispatch).toHaveBeenCalled();
+  });
+
+  it('should set the current page', () => {
+    mockViewer.currentPageNumber = 1;
+    wrapper.setPageNumber(2);
+    expect(mockViewer.currentPageNumber).toEqual(2);
+  });
+
+  it('should change the current page', () => {
+    mockViewer.currentPageNumber = 1;
+    wrapper.changePageNumber(-2);
+    expect(mockViewer.currentPageNumber).toEqual(-1);
   });
 });
