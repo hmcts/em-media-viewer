5de8928f16aedee7cd51f2999106120a03a0f938
Linus Norton <linusnorton@gmail.com>
2019-06-04 12:03:45 +0100

    rename objectId to documentId

--- projects/media-viewer/src/lib/media-viewer/annotations/annotation-set.model.ts
+++ projects/media-viewer/src/lib/media-viewer/annotations/annotation-set.model.ts
@@ -2,6 +2,6 @@ import { ApiPersisted } from './api-persisted.model';
 import { Annotation } from './annotation.model';
 
 export interface AnnotationSet extends ApiPersisted {
-  objectId: string;
+  documentId: string;
   annotations: Annotation[];
 }
