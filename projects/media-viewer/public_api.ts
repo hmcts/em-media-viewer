482a3725fb366ebb4ad2a001a2df7b69bba57f17
Linus Norton <linusnorton@gmail.com>
2019-06-04 15:23:08 +0100

    more restructuring

---
+++ projects/media-viewer/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of media-viewer
+ */
+
+export * from './src/media-viewer.module';
+export * from './src/toolbar/toolbar.module';
+export * from './src/events/action-events';
