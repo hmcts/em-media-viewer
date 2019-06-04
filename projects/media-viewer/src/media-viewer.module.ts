6988c187f878cf6c4157f8fede9ea7546d049a3b
Linus Norton <linusnorton@gmail.com>
2019-06-04 15:23:08 +0100

    more restructuring

--- projects/media-viewer/src/media-viewer.module.ts
+++ projects/media-viewer/src/media-viewer.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { PdfViewerComponent } from './media-viewer/viewers/pdf-viewer/pdf-viewer.component';
+import { PdfViewerComponent } from './viewers/pdf-viewer/pdf-viewer.component';
 import { NgtUniversalModule } from '@ng-toolkit/universal';
-import { ImageViewerComponent } from './media-viewer/viewers/image-viewer/image-viewer.component';
-import { UnsupportedViewerComponent } from './media-viewer/viewers/unsupported-viewer/unsupported-viewer.component';
-import { MediaViewerComponent } from './media-viewer/media-viewer.component';
-import { ToolbarModule } from './media-viewer/toolbar/toolbar.module';
-import { PdfJsWrapperFactory } from './media-viewer/viewers/pdf-viewer/pdf-js/pdf-js-wrapper.provider';
-import { AnnotationsModule } from './media-viewer/annotations/annotations.module';
+import { ImageViewerComponent } from './viewers/image-viewer/image-viewer.component';
+import { UnsupportedViewerComponent } from './viewers/unsupported-viewer/unsupported-viewer.component';
+import { MediaViewerComponent } from './media-viewer.component';
+import { ToolbarModule } from './toolbar/toolbar.module';
+import { PdfJsWrapperFactory } from './viewers/pdf-viewer/pdf-js/pdf-js-wrapper.provider';
+import { AnnotationsModule } from './annotations/annotations.module';
 
 @NgModule({
   imports: [
--- projects/media-viewer/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of media-viewer
- */
-
-export * from './lib/media-viewer.module';
-export * from './lib/media-viewer/toolbar/toolbar.module';
-export * from './lib/media-viewer/events/action-events';
