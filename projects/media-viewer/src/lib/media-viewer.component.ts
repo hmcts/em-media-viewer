462c3c784681005c2462e735185bd286c5d03a27
velimd <velican.duzgunce@solirius.com>
2019-06-07 12:10:54 +0100

    refreshing the viewer when change has occurred.

--- projects/media-viewer/src/lib/media-viewer.component.ts
+++ projects/media-viewer/src/lib/media-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Subject } from 'rxjs';
 import { ActionEvents } from './events/action-events';
 import { getToolbarButtonToggles, ToolbarButtonToggles } from './events/toolbar-button-toggles';
@@ -9,7 +9,7 @@ import { SetCurrentPageOperation } from './events/viewer-operations';
   templateUrl: './media-viewer.component.html',
   styleUrls: ['styles/main.scss']
 })
-export class MediaViewerComponent implements OnInit {
+export class MediaViewerComponent implements OnInit, OnChanges {
 
   @Input() url: string;
   @Input() downloadFileName: string;
@@ -26,6 +26,10 @@ export class MediaViewerComponent implements OnInit {
     if (!this.toolbarButtonToggles) {
       this.toolbarButtonToggles = getToolbarButtonToggles(this.contentType);
     }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('Media Viewer Component has changed');
   }
 
   contentTypeUnsupported(): boolean {
