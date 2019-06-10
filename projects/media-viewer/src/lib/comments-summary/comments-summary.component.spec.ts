c61584f5bc57dacb00155d401b191a6eaeae8331
Linus Norton <linusnorton@gmail.com>
2019-06-10 12:30:16 +0100

    another test

--- projects/media-viewer/src/lib/comments-summary/comments-summary.component.spec.ts
+++ projects/media-viewer/src/lib/comments-summary/comments-summary.component.spec.ts
@@ -2,10 +2,13 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CommentsSummaryComponent } from './comments-summary.component';
 import { PrintService } from '../print.service';
+import { Subject } from 'rxjs';
+import { PdfViewerComponent } from '../viewers/pdf-viewer/pdf-viewer.component';
 
 describe('CommentsSummaryComponent', () => {
   let component: CommentsSummaryComponent;
   let fixture: ComponentFixture<CommentsSummaryComponent>;
+  let printService: PrintService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,6 +21,7 @@ describe('CommentsSummaryComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CommentsSummaryComponent);
     component = fixture.componentInstance;
+    printService = TestBed.get(PrintService);
 
     fixture.detectChanges();
   });
@@ -25,4 +29,22 @@ describe('CommentsSummaryComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('close', async(() => {
+    component.showCommentSummary = new Subject<boolean>();
+    component.showCommentSummary.subscribe(value => {
+      expect(value).toEqual(false);
+    });
+
+    component.onClose();
+  }));
+
+  it('print', () => {
+    const printSpy = spyOn(printService, 'printElementNatively').and.stub();
+
+    component.onPrint();
+
+    expect(printSpy).toHaveBeenCalled();
+  });
+
 });
