d3a2330f87ce1e209466a783d8c54062e2a20398
Linus Norton <linusnorton@gmail.com>
2019-06-19 15:36:20 +0100

    Added popup-toolbar (#75)

---
+++ projects/media-viewer/src/lib/annotations/rectangle/popup-toolbar/popup-toolbar.component.spec.ts
@@ -0,0 +1,34 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { PopupToolbarComponent } from './popup-toolbar.component';
+import { By } from '@angular/platform-browser';
+
+describe('PopupToolbarComponent', () => {
+  let component: PopupToolbarComponent;
+  let fixture: ComponentFixture<PopupToolbarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PopupToolbarComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PopupToolbarComponent);
+    component = fixture.componentInstance;
+    component.top = 100;
+    component.left = 100;
+
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('adjust its position', () => {
+    const element = fixture.debugElement.query(By.css('div'));
+    console.log(element.styles);
+
+    expect(element.styles.left).toEqual((component.left - 100 ) + 'px');
+    expect(element.styles.top).toEqual((component.top - 70) + 'px');
+  });
+});
