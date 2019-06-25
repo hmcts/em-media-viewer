0d95cd0a64a704f7b071e69e9a8ebb7fea3793db
Russell Ward <russell.ward@inkltd.co.uk>
2019-06-25 13:22:06 +0100

    EM1743 - Created a Utility service that can be used within the pdf or image viewer for translating mouse-coordinates to the localElement X and Y (relative to their container). This allows for absolutely positioning of elements - prep work for TextSelection and Draw Modes

---
+++ projects/media-viewer/src/lib/utils/mouse-selection-utils.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MouseSelectionUtils {
+
+  // Return the absolute positioning of the top, left x & y coordinates relative to the localElement
+  static getTargetElementAbsoluteMouseCoordinates(event: MouseEvent): { x: number, y: number } {
+    const responseObject = {
+      x: event.x,
+      y: event.y
+    };
+
+    // Get the VIEWPORT-relative coordinates of the click.
+    const viewportX: number = event.clientX;
+    const viewportY: number = event.clientY;
+    //
+    // Obtain the HTMLElement from the event
+    const localElement: Element = (<Element>event.target) || (<Element>event.srcElement);
+    if (!localElement) {
+      return responseObject;
+    }
+    // Now that we have the VIEWPORT coordinates of the CLICK, we need to get the
+    // VIEWPORT position of the target element.
+    const boxRectangle = localElement.getBoundingClientRect();
+
+    // Now that we have the targets VIEWPORT coordinates and the click's VIEWPORT
+    // coordinates, we can take the difference between the two in order to
+    // translate the VIEWPORT coordinates into target-LOCAL coordinates.
+    const localX: number = (viewportX - boxRectangle.left);
+    const localY: number = (viewportY - boxRectangle.top);
+
+    if (localX && localY) {
+      responseObject.x = localX;
+      responseObject.y = localY;
+      // console.log('Translating Viewport {', viewportX, ',', viewportY, '}', 'to Local {', localX, ',', localY, '}');
+    }
+    return responseObject;
+  }
+}
