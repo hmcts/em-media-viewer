441e952482e8e9275b7ff82022e9ff19c36fcdbf
Pawel Kaczmarek <pawel.kacmarek@solirius.com>
2019-06-26 14:20:45 +0200

    unit test fixes

--- projects/media-viewer/src/lib/annotations/annotation-set/annotation/rectangle/popup-toolbar/popup-toolbar.component.spec.ts
+++ projects/media-viewer/src/lib/annotations/annotation-set/annotation/rectangle/popup-toolbar/popup-toolbar.component.spec.ts
@@ -5,6 +5,25 @@ import { By } from '@angular/platform-browser';
 describe('PopupToolbarComponent', () => {
   let component: PopupToolbarComponent;
   let fixture: ComponentFixture<PopupToolbarComponent>;
+  const mockRectangle = {
+    x: 100, y: 100, width: 100, height: 20,
+    id: '16d5c513-15f9-4c39-8102-88bdb85d8831',
+    annotationId: '4f3f9361-6d17-4689-81dd-5cb2e317b329',
+    createdDate: '2018-05-28T08:48:33.206Z',
+    createdBy: 'ea6d959c-b6c9-48af-89c2-6f7bd796524d',
+    createdByDetails: {
+      'forename': 'Linus',
+      'surname': 'Norton',
+      'email': 'linus.norton@hmcts.net'
+    },
+    lastModifiedDate: '2019-05-28T08:48:33.206Z',
+    lastModifiedBy: 'ea6d959c-b6c9-48af-89c2-6f7bd796524d',
+    lastModifiedByDetails: {
+      'forename': 'Jeroen',
+      'surname': 'Rijks',
+      'email': 'jeroen.rijks@hmcts.net'
+    },
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,8 +33,7 @@ describe('PopupToolbarComponent', () => {
 
     fixture = TestBed.createComponent(PopupToolbarComponent);
     component = fixture.componentInstance;
-    component.top = 100;
-    component.left = 100;
+    component.rectangle = mockRectangle;
 
     fixture.detectChanges();
   }));
@@ -26,9 +44,7 @@ describe('PopupToolbarComponent', () => {
 
   it('adjust its position', () => {
     const element = fixture.debugElement.query(By.css('div'));
-    console.log(element.styles);
-
-    expect(element.styles.left).toEqual((component.left - 175 ) + 'px');
-    expect(element.styles.top).toEqual((component.top - 70) + 'px');
+    expect(element.styles.left).toEqual('-25px');
+    expect(element.styles.top).toEqual('30px');
   });
 });
