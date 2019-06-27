8f22594bd047c9c70ec865074c729748a94f9718
Linus Norton <linusnorton@gmail.com>
2019-06-27 09:50:17 +0100

    Fix selection (#89)

--- projects/media-viewer/src/lib/annotations/annotation-set/annotation-set.component.spec.ts
+++ projects/media-viewer/src/lib/annotations/annotation-set/annotation-set.component.spec.ts
@@ -5,19 +5,21 @@ import { RectangleComponent } from './annotation/rectangle/rectangle.component';
 import { FormsModule } from '@angular/forms';
 import { AngularDraggableModule } from 'angular2-draggable';
 import { annotationSet } from '../../../assets/annotation-set';
-import { Subject } from 'rxjs';
 import { PopupToolbarComponent } from './annotation/rectangle/popup-toolbar/popup-toolbar.component';
-import {AnnotationComponent} from './annotation/annotation.component';
-import { AnnotationsModule } from '../annotations.module';
+import { AnnotationComponent } from './annotation/annotation.component';
 import { AnnotationApiService } from '../annotation-api.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import { ElementRef } from '@angular/core';
 
 describe('AnnotationSetComponent', () => {
   let component: AnnotationSetComponent;
   let fixture: ComponentFixture<AnnotationSetComponent>;
 
-  beforeEach(async(() => {
-    return TestBed.configureTestingModule({
+  const api = new AnnotationApiService({}  as any);
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [
         AnnotationSetComponent,
         AnnotationComponent,
@@ -31,13 +33,10 @@ describe('AnnotationSetComponent', () => {
         HttpClientTestingModule
       ],
       providers: [
-        AnnotationApiService
+        { provide: AnnotationApiService, useValue: api }
       ]
-    })
-      .compileComponents();
-  }));
+    }).compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(AnnotationSetComponent);
     component = fixture.componentInstance;
     component.annotationSet = annotationSet;
@@ -47,6 +46,57 @@ describe('AnnotationSetComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('update annotations', () => {
+    const spy = spyOn(api, 'postAnnotationSet').and.returnValues(of(annotationSet));
+    const annotation = { ...annotationSet.annotations[0] };
+
+    annotation.color = 'red';
+    component.onAnnotationUpdate(annotation);
+
+    annotationSet.annotations[0] = annotation;
+    expect(spy).toHaveBeenCalledWith(annotationSet);
+  });
+
+  it('select an annotation', () => {
+    component.onAnnotationSelected(true, 1);
+
+    expect(component.selected).toEqual(1);
+  });
+
+  it('deselects an annotation', () => {
+    component.onAnnotationSelected(false, 1);
+
+    expect(component.selected).toEqual(-1);
+  });
+
+  it('starts drawing on mousedown', () => {
+    component.newRectangle = new ElementRef(document.createElement('div'));
+    component.drawMode = true;
+    component.onMouseDown({ pageY: 10, pageX: 10 } as MouseEvent);
+
+    expect(component.newRectangle.nativeElement.style.left).toEqual('10px');
+    expect(component.newRectangle.nativeElement.style.top).toEqual('10px');
+  });
+
+  it('finishes drawing on mouseup', () => {
+    const annotation = Object.assign({}, annotationSet.annotations[0]);
+    annotation.id = 'new';
+
+    const spy = spyOn(api, 'postAnnotation').and.returnValues(of(annotation));
+
+    component.newRectangle = new ElementRef(document.createElement('div'));
+    component.drawMode = true;
+    component.selected = 1;
+    component.onMouseDown({ pageY: 10, pageX: 10 } as MouseEvent);
+    component.onMouseMove({ pageY: 100, pageX: 100 } as MouseEvent);
+    component.onMouseUp();
+
+    expect(spy).toHaveBeenCalled();
+
+    expect(component.annotationSet.annotations[component.annotationSet.annotations.length - 1].id).toEqual('new');
+    expect(component.selected).toEqual(-1);
   });
 
 });
--- projects/media-viewer/src/lib/annotations/annotation-set/annotation-set.component.ts
+++ projects/media-viewer/src/lib/annotations/annotation-set/annotation-set.component.ts
@@ -1,7 +1,9 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { Annotation } from './annotation/annotation.model';
 import { AnnotationApiService } from '../annotation-api.service';
 import { AnnotationSet } from './annotation-set.model';
+import { Rectangle } from './annotation/rectangle/rectangle.model';
+import uuid from 'uuid';
 
 @Component({
   selector: 'mv-annotation-set',
@@ -16,7 +18,16 @@ export class AnnotationSetComponent {
   @Input() width: number;
   @Input() height: number;
   @Input() page: number;
+  @Input() drawMode: boolean;
+
   @Output() update = new EventEmitter<Annotation>();
+
+  @ViewChild('newRectangle') newRectangle: ElementRef;
+  @ViewChild('container') container: ElementRef;
+
+  selected = -1;
+  drawStartX = -1;
+  drawStartY = -1;
 
   constructor(
     private readonly api: AnnotationApiService
@@ -33,5 +44,70 @@ export class AnnotationSetComponent {
 
     this.annotationSet.annotations = annotations;
     this.api.postAnnotationSet(this.annotationSet);
+  }
+
+  public onAnnotationSelected(selected: boolean, i: number) {
+    this.selected = selected ? i : -1;
+  }
+
+  public onMouseDown(event: MouseEvent) {
+    if (this.annotationSet && this.drawMode) {
+      this.drawStartX = event.pageX - (window.scrollX + this.container.nativeElement.getBoundingClientRect().left);
+      this.drawStartY = event.pageY - (window.scrollY + this.container.nativeElement.getBoundingClientRect().top);
+
+      this.newRectangle.nativeElement.style.display = 'block';
+      this.newRectangle.nativeElement.style.top =  this.drawStartY + 'px';
+      this.newRectangle.nativeElement.style.left = this.drawStartX + 'px';
+    }
+
+    return false;
+  }
+
+  public onMouseMove(event: MouseEvent) {
+    if (this.annotationSet && this.drawMode && this.drawStartX > 0 && this.drawStartY > 0) {
+      this.newRectangle.nativeElement.style.height =
+        (event.pageY - this.drawStartY - (window.scrollY + this.container.nativeElement.getBoundingClientRect().top)) + 'px';
+      this.newRectangle.nativeElement.style.width =
+        (event.pageX - this.drawStartX - (window.scrollX + this.container.nativeElement.getBoundingClientRect().left)) + 'px';
+    }
+
+    return false;
+  }
+
+  public onMouseUp() {
+    if (this.annotationSet && this.drawMode) {
+      const rectangle = {
+        id: uuid(),
+        x: +this.newRectangle.nativeElement.style.left.slice(0, -2),
+        y: +this.newRectangle.nativeElement.style.top.slice(0, -2),
+        width: +this.newRectangle.nativeElement.style.width.slice(0, -2),
+        height: +this.newRectangle.nativeElement.style.height.slice(0, -2),
+      };
+
+      const annotation = {
+        id: uuid(),
+        annotationSetId: this.annotationSet.id,
+        color: 'FFFF00',
+        comments: [],
+        page: 1,
+        rectangles: [rectangle as Rectangle],
+        type: 'highlight'
+      };
+
+      this.api
+        .postAnnotation(annotation)
+        .subscribe(a => this.annotationSet.annotations.push(a));
+
+      this.drawStartX = -1;
+      this.drawStartY = -1;
+      this.newRectangle.nativeElement.style.display = 'none';
+      this.newRectangle.nativeElement.style.width = '0';
+      this.newRectangle.nativeElement.style.height = '0';
+
+    }
+
+    this.selected = -1;
+
+    return false;
   }
 }
