452a26bb6650611a01101ba42c5b77cd16a8e6b1
PhaniP <phani.perla@hmcts.net>
2019-07-03 10:26:07 +0100

    Code  improvements

--- e2e/src/pages/navigate.po.ts
+++ e2e/src/pages/navigate.po.ts
@@ -2,22 +2,34 @@ import {by, element, ElementFinder} from 'protractor';
 import { AppPage } from './app.po';
 
 export class NavigatePage extends AppPage {
+   public pageNumber: ElementFinder;
+   public next: ElementFinder;
+   public previous: ElementFinder;
+
+  constructor () {
+    super();
+    console.log('Navigate Page Constructor');
+
+    this.next = element(by.id('next'));
+    this.previous = element(by.id('previous'));
+    this.pageNumber = element(by.id('pageNumber'));
+
+  }
+
+
+  async number() {
+    // await this.waitForElement(by.id('pageNumber'));
+    // return element(by.id('pageNumber')).getAttribute('value');
+  }
 
   async goToNextPage() {
-    await this.waitForElement(by.id('next'));
-    await this.clickElement(by.id('next'));
+    console.log('Navigate Page Method');
+    // await this.waitForElement(by.id('next'));
+    // await this.clickElement(by.id('next'));
   }
 
   async goToPreviousPage() {
     await this.waitForElement(by.id('previous'));
     await this.clickElement(by.id('previous'));
-  }
-
-  async number() {
-    // await this.waitForElement(by.id('pageNumber'));
-    const  page = element(by.xpath('//*[@id="pageNumber"]')).getAttribute('value');
-    console.log('Page Number ' + page)
-    console.log(element(by.xpath('//*[@id="pageNumber"]')).getAttribute('value'));
-    return await element(by.xpath('//*[@id="pageNumber"]')).getAttribute('value');
   }
 }
