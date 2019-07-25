925c6c0985dcdafd61ba197e077c823c320f05f8
velimd <velican.duzgunce@solirius.com>
2019-07-25 12:52:27 +0100

    removing ngdraggable

--- projects/media-viewer/src/lib/annotations/annotation-set/annotation/rectangle/rectangle.component.ts
+++ projects/media-viewer/src/lib/annotations/annotation-set/annotation/rectangle/rectangle.component.ts
@@ -1,6 +1,5 @@
 import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { Rectangle } from './rectangle.model';
-import { IResizeEvent } from 'angular2-draggable/lib/models/resize-event';
 
 @Component({
   selector: 'mv-anno-rectangle',
@@ -13,7 +12,6 @@ export class RectangleComponent {
   @Input() color: String;
   @Input() zoom: number;
   @Input() rotate: number;
-  @Input() draggable: boolean;
 
   @Output() click = new EventEmitter();
   @Output() update = new EventEmitter<Rectangle>();
@@ -35,25 +33,5 @@ export class RectangleComponent {
 
   onClick() {
     this.click.emit();
-  }
-
-  onMove(el: HTMLElement) {
-    const [left, top] = el.style.transform.match(/\d+/g);
-
-    const newX = +left / this.zoom;
-    const newY = +top / this.zoom;
-
-    if (Math.abs(newX - this.rectangle.x) > 2 || Math.abs(newY - this.rectangle.y)) {
-      this.rectangle.x = newX;
-      this.rectangle.y = newY;
-      this.update.emit(this.rectangle);
-    }
-  }
-
-  onResize(event: IResizeEvent) {
-    this.rectangle.width = event.size.width / this.zoom;
-    this.rectangle.height = event.size.height / this.zoom;
-
-    this.update.emit(this.rectangle);
   }
 }
