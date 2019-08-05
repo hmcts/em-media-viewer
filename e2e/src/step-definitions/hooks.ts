0176ce893aab598d12f4bb5650a69ccc0af3ddd1
PhaniP <phani.perla@hmcts.net>
2019-08-05 15:18:29 +0100

    Added Annotations stub command

--- e2e/src/step-definitions/hooks.ts
+++ e2e/src/step-definitions/hooks.ts
@@ -1,21 +1,21 @@
 import {After, AfterAll, Before, BeforeAll, Status} from 'cucumber';
 import {browser} from 'protractor';
-// import pact, {Server} from '@pact-foundation/pact-node';
-//
-// let pactMockServer: Server;
-//
-// BeforeAll(async function() {
-//   pactMockServer = pact.createStub({
-//     port: 9999,
-//     pactUrls: ['/Users/pawel/workspace/media-viewer/pacts/media_viewer-annotation_api.json']
-//   });
-//   await pactMockServer.start();
-// });
-//
-//
-// AfterAll(async function() {
-//   pactMockServer.stop();
-// });
+import pact, {Server} from '@pact-foundation/pact-node';
+
+let pactMockServer: Server;
+
+BeforeAll({tags: '@MediaViewer'}, async function() {
+  pactMockServer = pact.createStub({
+    port: 9999,
+    pactUrls: ['/Users/pawel/workspace/media-viewer/pacts/media_viewer-annotation_api.json']
+  });
+  await pactMockServer.start();
+});
+
+
+AfterAll(async function() {
+  pactMockServer.stop();
+});
 
 
 Before({tags: '@MediaViewer'}, async function() {
