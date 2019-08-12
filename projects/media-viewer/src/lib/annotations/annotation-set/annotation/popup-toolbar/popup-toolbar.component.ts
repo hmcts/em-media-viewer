3a05282dd61980c8ccdb67394eff8ba8adae3629
Tom Elliott <tomxelliott@hotmail.co.uk>
2019-08-12 09:48:34 +0100

    left box changes

--- projects/media-viewer/src/lib/annotations/annotation-set/annotation/popup-toolbar/popup-toolbar.component.ts
+++ projects/media-viewer/src/lib/annotations/annotation-set/annotation/popup-toolbar/popup-toolbar.component.ts
@@ -21,33 +21,37 @@ export class PopupToolbarComponent {
   popupStyles() {
     if (this.rotate === 0) {
       const top = this.rectangle.y * this.zoom - this.HEIGHT;
+      const left = ((this.rectangle.x + (this.rectangle.width / 2)) * this.zoom - (this.WIDTH / 2));
       return {
         top: (top <= 0 ? this.HEIGHT : top) + 'px',
-        left: ((this.rectangle.x + (this.rectangle.width / 2)) * this.zoom - (this.WIDTH / 2)) + 'px'
+        left: (left <= 0 ? 0 : left) + 'px'
       };
     } else if (this.rotate === 90) {
       const top = this.rectangle.y * this.zoom + this.rectangle.height / 2 * this.zoom + this.WIDTH / 2;
+      const left = (this.rectangle.x * this.zoom) - (this.HEIGHT);
       return {
         transform: 'rotate(-90deg)',
         'transform-origin': 'top left',
         top: (top <= 0 ? this.HEIGHT : top) + 'px',
-        left: (this.rectangle.x * this.zoom) - (this.HEIGHT) + 'px'
+        left: (left <= 0 ? 0 : left) + 'px'
       };
     } else if (this.rotate === 180) {
       const top = (this.rectangle.y + this.rectangle.height) * this.zoom + 10;
+      const left = ((this.rectangle.x + (this.rectangle.width / 2)) * this.zoom - (this.WIDTH / 2));
       return {
         transform: 'rotate(-180deg)',
         'transform-origin': 'center center',
         top: (top <= 0 ? this.HEIGHT : top)  + 'px',
-        left: ((this.rectangle.x + (this.rectangle.width / 2)) * this.zoom - (this.WIDTH / 2)) + 'px'
+        left: (left <= 0 ? 0 : left) + 'px'
       };
     } else if (this.rotate === 270) {
       const top = (this.rectangle.y + (this.rectangle.height / 2)) * this.zoom - (this.WIDTH / 2);
+      const left = (this.rectangle.x + (this.rectangle.width)) * this.zoom + this.HEIGHT;
       return {
         transform: 'rotate(-270deg)',
         'transform-origin': 'top left',
         top: (top <= 0 ? this.HEIGHT : top) + 'px',
-        left: (this.rectangle.x + (this.rectangle.width)) * this.zoom + this.HEIGHT + 'px'
+        left: (left <= 0 ? 0 : left) + 'px'
       };
     }
   }
