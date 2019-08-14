d58243b8cbec636ef713b21dfb56566cc2500bc8
velimd <velican.duzgunce@solirius.com>
2019-08-14 16:20:24 +0100

    exporting toolbar services

--- projects/media-viewer/src/public_api.ts
+++ projects/media-viewer/src/public_api.ts
@@ -7,3 +7,4 @@ export * from './lib/toolbar/toolbar.module';
 export * from './lib/comments-summary/comments-summary.module';
 export * from './lib/annotations/annotation-api.service';
 export * from './lib/annotations/annotations.module';
+export * from './lib/toolbar/toolbar.module';
