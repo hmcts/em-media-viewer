abbba32a98901e3ce95affb14ff813e39a74325b
velimd <velican.duzgunce@solirius.com>
2019-08-15 15:31:06 +0100

    fix unit test

--- projects/media-viewer/src/lib/annotations/annotation-set/annotation/rectangle/rectangle.component.spec.ts
+++ projects/media-viewer/src/lib/annotations/annotation-set/annotation/rectangle/rectangle.component.spec.ts
@@ -87,7 +87,7 @@ describe('RectangleComponent', () => {
 
   it('should select the rectangle if select is true.', () => {
     spyOn(component.rectElement.nativeElement, 'focus');
-    component.selected = true;
+    component._selected = true;
     component.ngAfterViewInit();
     expect(component.rectElement.nativeElement.focus).toHaveBeenCalledTimes(1);
   });
