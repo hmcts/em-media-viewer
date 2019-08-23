26edaa508060a581253e7900816f61c5f3bd3502
PhaniP <phani.perla@hmcts.net>
2019-08-23 13:37:50 +0100

    Zoom and Rotate scenarios EM-1993

--- e2e/src/step_definitions/hooks.ts
+++ e2e/src/step_definitions/hooks.ts
@@ -1,15 +1,23 @@
-import {After, Before, Status} from 'cucumber';
+import {After, AfterAll, Before, BeforeAll, Status} from 'cucumber';
 import {browser} from 'protractor';
 
-Before({tags: '@MediaViewer and ~@WIP'}, async function() {
+// BeforeAll({timeout: 60 * 1000}, async () => {
+//   await browser.driver.navigate().to(browser.baseUrl);
+// });
+
+Before(async function() {
   await browser.waitForAngularEnabled(false);
   await browser.driver.navigate().to(browser.baseUrl);
 });
 
 
-After(async function(scenario) {
+After(async function (scenario) {
   if (scenario.result.status === Status.FAILED) {
-    const screenshot = await browser.takeScreenshot();
-    this.attach(screenshot, 'image/png');
+    const screenshots = await browser.takeScreenshot();
+    this.attach(screenshots, 'image/png');
   }
+
+  // AfterAll({timeout: 60 * 1000}, async () => {
+  //   await browser.quit();
+  // });
 });
