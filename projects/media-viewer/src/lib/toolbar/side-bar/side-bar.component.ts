4435b9eea2e2330f5f1ef8719f52e8efa7fb9042
Tom Elliott <tomxelliott@hotmail.co.uk>
2019-08-27 16:56:44 +0100

    EM-1387: Make changes after show and tell

--- projects/media-viewer/src/lib/toolbar/side-bar/side-bar.component.ts
+++ projects/media-viewer/src/lib/toolbar/side-bar/side-bar.component.ts
@@ -1,52 +1,15 @@
-import {AfterContentInit, Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mv-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['../../styles/main.scss']
 })
-export class SideBarComponent implements OnChanges, AfterContentInit {
+export class SideBarComponent {
 
-  private showThumbnailView: boolean;
-  private showOutlineView: boolean;
-  private showAttachmentsView: boolean;
 
   @Input() url: string;
 
   constructor() {
-  }
-
-  ngAfterContentInit() {
-    this.showThumbnailView = true;
-    this.showOutlineView = false;
-    this.showAttachmentsView = false;
-  }
-
-  async ngOnChanges(changes: SimpleChanges) {
-    this.reset();
-  }
-
-  reset() {
-    this.showThumbnailView = true;
-    this.showOutlineView = false;
-    this.showAttachmentsView = false;
-  }
-
-  selectThumbnailView() {
-    this.showThumbnailView = true;
-    this.showOutlineView = false;
-    this.showAttachmentsView = false;
-  }
-
-  selectOutlineView() {
-    this.showThumbnailView = false;
-    this.showOutlineView = true;
-    this.showAttachmentsView = false;
-  }
-
-  selectAttachmentView() {
-    this.showThumbnailView = false;
-    this.showOutlineView = false;
-    this.showAttachmentsView = true;
   }
 }
