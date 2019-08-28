abeaf3e60f0fe292e3cc855cf17ea62aa44522e7
Tom Elliott <tomxelliott@hotmail.co.uk>
2019-08-28 14:02:35 +0100

    EM-1387: Move code to render the outline into the media viewer code

--- projects/media-viewer/src/lib/toolbar/side-bar/outline-view/outline-view.component.ts
+++ projects/media-viewer/src/lib/toolbar/side-bar/outline-view/outline-view.component.ts
@@ -45,13 +45,126 @@ export class OutlineViewComponent implements OnChanges, AfterContentInit {
     loadingTask.then(pdf => {
       pdf.getOutline().then((outline) => {
         this.outline = outline;
-        this.pdfDocumentOutline.render({outline: this.outline, });
-        this.generateOutlineStructure(this.outline);
+        this.render({outline: this.outline, });
       });
     });
   }
 
-  generateOutlineStructure(outline) {
-    console.log(outline);
+  _dispatchEvent(outlineCount) {
+    this.pdfDocumentOutline.eventBus.dispatch('outlineloaded', {
+      source: this,
+      outlineCount,
+    });
+  }
+
+  _bindLink(element, { url, newWindow, dest, }) {
+    const linkService = this.pdfDocumentOutline.linkService;
+    if (url) {
+      pdfjsLib.addLinkAttributes(element, {
+        url,
+        target: (newWindow ? pdfjsLib.LinkTarget.BLANK : linkService.externalLinkTarget),
+        rel: linkService.externalLinkRel,
+        enabled: linkService.externalLinkEnabled,
+      });
+      return;
+    }
+
+    element.href = linkService.getDestinationHash(dest);
+    element.onclick = () => {
+      if (dest) {
+        linkService.navigateTo(dest);
+      }
+      return false;
+    };
+  }
+
+  _setStyles(element, { bold, italic, }) {
+    let styleStr = '';
+    if (bold) {
+      styleStr += 'font-weight: bold;';
+    }
+    if (italic) {
+      styleStr += 'font-style: italic;';
+    }
+    if (styleStr) {
+      element.setAttribute('style', styleStr);
+    }
+  }
+
+  _addToggleButton(div, { count, items, }) {
+    const toggler = document.createElement('div');
+    toggler.className = 'outlineItemToggler outlineItemsHidden';
+    if (count < 0 && Math.abs(count) === items.length) {
+      toggler.classList.add('outlineItemsHidden');
+    }
+    toggler.onclick = (evt) => {
+      evt.stopPropagation();
+      toggler.classList.toggle('outlineItemsHidden');
+
+      if (evt.shiftKey) {
+        const shouldShowAll = !toggler.classList.contains('outlineItemsHidden');
+        this._toggleOutlineItem(div, shouldShowAll);
+      }
+    };
+    div.insertBefore(toggler, div.firstChild);
+  }
+
+  _toggleOutlineItem(root, show = false) {
+    this.pdfDocumentOutline.lastToggleIsShow = show;
+    for (const toggler of root.querySelectorAll('.outlineItemToggler')) {
+      toggler.classList.toggle('outlineItemsHidden', !show);
+    }
+  }
+
+  render({ outline, }) {
+    const DEFAULT_TITLE = '\u2013';
+    let outlineCount = 0;
+
+    if (this.outline) {
+      this.pdfDocumentOutline.reset();
+    }
+    this.outline = outline || null;
+
+    if (!outline) {
+      this._dispatchEvent(outlineCount);
+      return;
+    }
+
+    const fragment = document.createDocumentFragment();
+    const queue = [{ parent: fragment, items: this.outline, }];
+    let hasAnyNesting = false;
+    while (queue.length > 0) {
+      const levelData = queue.shift();
+      for (const item in levelData.items) {
+        const div = document.createElement('div');
+        div.className = 'outlineItem';
+
+        const element = document.createElement('a');
+        this._bindLink(element, levelData.items[item]);
+        this._setStyles(element, levelData.items[item]);
+        element.textContent = pdfjsLib.removeNullCharacters(levelData.items[item].title) || DEFAULT_TITLE;
+
+        div.appendChild(element);
+
+        if (levelData.items[item].items.length > 0) {
+          hasAnyNesting = true;
+          this._addToggleButton(div, levelData.items[item]);
+
+          const itemsDiv = document.createElement('div');
+          itemsDiv.className = 'outlineItems';
+          div.appendChild(itemsDiv);
+          queue.push({ parent: <DocumentFragment><unknown>itemsDiv, items: levelData.items[item].items, });
+        }
+        levelData.parent.appendChild(div);
+        outlineCount++;
+      }
+    }
+    if (hasAnyNesting) {
+      this.pdfDocumentOutline.container.classList.add('outlineWithDeepNesting');
+      this.pdfDocumentOutline.lastToggleIsShow =
+        (fragment.querySelectorAll('.outlineItemsHidden').length === 0);
+    }
+    this.pdfDocumentOutline.container.appendChild(fragment);
+    this._dispatchEvent(outlineCount);
   }
 }
