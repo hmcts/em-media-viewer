c860dd8f3e05a254f0e0afd1f29be1aa57d701b4
benouaer <tech@benouaer.com>
2019-09-04 14:42:44 +0100

    remove constant initialisation from declaration to fix TS2.8 issue

--- projects/media-viewer/src/lib/annotations/annotation-set/annotation/popup-toolbar/popup-toolbar.component.ts
+++ projects/media-viewer/src/lib/annotations/annotation-set/annotation/popup-toolbar/popup-toolbar.component.ts
@@ -8,8 +8,8 @@ import { Rectangle} from '../rectangle/rectangle.model';
 })
 export class PopupToolbarComponent {
 
-  readonly HEIGHT = 70;
-  readonly WIDTH = 350;
+  readonly defaultHeight;
+  readonly defaultWidth;
 
   @Input() rectangle: Rectangle;
   @Input() zoom = 1;
@@ -19,41 +19,66 @@ export class PopupToolbarComponent {
   @Output() deleteHighlight = new EventEmitter();
   @Output() addOrEditComment = new EventEmitter();
 
-  popupStyles() {
+  constructor() {
+    this.defaultHeight = 70;
+    this.defaultWidth = 350;
+  }
+
+  get style() {
+    const style: any = {
+      top: this.top + 'px',
+      left: this.left + 'px'
+    };
+    if (this.rotate === 90) {
+      style.transform = 'rotate(-90deg)';
+      style['transform-origin'] = 'top left';
+    } else if (this.rotate === 180) {
+      style.transform = 'rotate(-180deg)';
+      style['transform-origin'] = 'center center';
+    } else if (this.rotate === 270) {
+      style.transform = 'rotate(-270deg)';
+      style['transform-origin'] = 'top left';
+    }
+    return style;
+  }
+
+  get top() {
     if (this.rotate === 0) {
-      const top = this.rectangle.y * this.zoom - this.HEIGHT;
-      const left = ((this.rectangle.x + (this.rectangle.width / 2)) * this.zoom - (this.WIDTH / 2));
-      return {
-        top: (top <= 0 ? this.HEIGHT : top) + 'px',
-        left: (left <= 0 ? 0 : left) + 'px'
-      };
+      const top = this.rectangle.y * this.zoom - this.defaultHeight;
+      return top <= 0 ? this.defaultHeight : top;
+
     } else if (this.rotate === 90) {
-      const top = this.rectangle.y * this.zoom + this.rectangle.height / 2 * this.zoom + this.WIDTH / 2;
-      const left = (this.rectangle.x * this.zoom) - (this.HEIGHT);
-      return {
-        transform: 'rotate(-90deg)',
-        'transform-origin': 'top left',
-        top: (top >= this.height ? this.height : top) + 'px',
-        left: (left <= 0 ? this.HEIGHT : left) + 'px'
-      };
+      const top = this.rectangle.y * this.zoom + this.rectangle.height/2 * this.zoom + this.defaultWidth/2;
+      return top >= this.height ? this.height : top;
+
     } else if (this.rotate === 180) {
       const top = (this.rectangle.y + this.rectangle.height) * this.zoom + 10;
-      const left = ((this.rectangle.x + (this.rectangle.width / 2)) * this.zoom - (this.WIDTH / 2));
-      return {
-        transform: 'rotate(-180deg)',
-        'transform-origin': 'center center',
-        top: (top >= this.height - this.HEIGHT ? this.height - this.HEIGHT : top)  + 'px',
-        left: (left >= this.width - this.WIDTH ? this.width - this.WIDTH : left) + 'px'
-      };
+      return top >= this.height - this.defaultHeight ? this.height - this.defaultHeight : top;
+
     } else if (this.rotate === 270) {
-      const top = (this.rectangle.y + (this.rectangle.height / 2)) * this.zoom - (this.WIDTH / 2);
-      const left = (this.rectangle.x + (this.rectangle.width)) * this.zoom + this.HEIGHT;
-      return {
-        transform: 'rotate(-270deg)',
-        'transform-origin': 'top left',
-        top: (top <= 0 ? 0 : top) + 'px',
-        left: (left >= this.width - this.HEIGHT ? this.width - this.HEIGHT : left) + 'px'
-      };
+      const top = (this.rectangle.y + (this.rectangle.height/2)) * this.zoom - (this.defaultWidth/2);
+      return top <= 0 ? 0 : top;
+
+    }
+  }
+
+  get left() {
+    if (this.rotate === 0) {
+      const left = (this.rectangle.x + (this.rectangle.width/2)) * this.zoom - (this.defaultWidth/2);
+      return left <= 0 ? 0 : left;
+
+    } else if (this.rotate === 90) {
+      const left = (this.rectangle.x * this.zoom) - (this.defaultHeight);
+      return left <= 0 ? this.defaultHeight : left;
+
+    } else if (this.rotate === 180) {
+      const left = ((this.rectangle.x + (this.rectangle.width/2)) * this.zoom - (this.defaultWidth/2));
+      return left >= this.width - this.defaultWidth ? this.width - this.defaultWidth : left;
+
+    } else if (this.rotate === 270) {
+      const left = (this.rectangle.x + (this.rectangle.width)) * this.zoom + this.defaultHeight;
+      return left >= this.width - this.defaultHeight ? this.width - this.defaultHeight : left;
+
     }
   }
 }
