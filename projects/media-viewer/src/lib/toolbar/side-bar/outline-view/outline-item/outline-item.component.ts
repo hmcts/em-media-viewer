62948b573cce7dc1b8d89fc91749c0fd6d53302f
velimd <velican.duzgunce@solirius.com>
2019-09-08 17:26:13 +0100

    rendering outline via angular

---
+++ projects/media-viewer/src/lib/toolbar/side-bar/outline-view/outline-item/outline-item.component.ts
@@ -0,0 +1,36 @@
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import { PdfJsWrapperFactory } from '../../../../viewers/pdf-viewer/pdf-js/pdf-js-wrapper.provider';
+import * as pdfjsOutlineViewer from 'pdfjs-dist/lib/web/pdf_outline_viewer';
+import { Outline } from '../outline.model';
+
+@Component({
+  selector: 'mv-outline-item',
+  templateUrl: './outline-item.component.html',
+  styleUrls: ['../../../../styles/main.scss'],
+})
+export class OutlineItemComponent implements OnInit {
+
+  private linkService: pdfjsOutlineViewer;
+  @Input() outline: Outline;
+
+  showOutlineItems: boolean;
+
+  @ViewChild('outlineItem') outlineItem: ElementRef;
+
+  constructor(private readonly pdfJsWrapperFactory: PdfJsWrapperFactory) { }
+
+  ngOnInit() {
+    this.showOutlineItems = false;
+    this.linkService = this.pdfJsWrapperFactory.getLinkService();
+  }
+
+  navigateLink() {
+    if (this.outline.dest) {
+      this.linkService.navigateTo(this.outline.dest);
+    }
+  }
+
+  toggleOutline() {
+    this.showOutlineItems = !this.showOutlineItems;
+  }
+}
--- projects/media-viewer/src/lib/toolbar/side-bar/outline-view/outline-view.component.ts
+++ projects/media-viewer/src/lib/toolbar/side-bar/outline-view/outline-view.component.ts
@@ -10,15 +10,13 @@ import { PdfJsWrapperFactory } from '../../../viewers/pdf-viewer/pdf-js/pdf-js-w
   selector: 'mv-outline-view',
   templateUrl: './outline-view.component.html',
   styleUrls: ['../../../styles/main.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class OutlineViewComponent implements OnChanges, AfterContentInit {
 
   @Input() url: string;
   @ViewChild('outlineContainer') outlineContainer: ElementRef;
 
-  private pdfDocumentOutline: pdfjsOutlineViewer.PDFOutlineViewer;
-  private outline: {};
+  private outline = [];
 
   constructor(private readonly pdfJsWrapperFactory: PdfJsWrapperFactory) {
   }
@@ -29,13 +27,12 @@ export class OutlineViewComponent implements OnChanges, AfterContentInit {
 
   async ngOnChanges(changes: SimpleChanges) {
     if (changes.url) {
-      this.outline = {};
+      this.outline = [];
       await this.renderOutlineView();
     }
   }
 
   async renderOutlineView() {
-    this.pdfDocumentOutline = this.pdfJsWrapperFactory.createDocumentOutline(this.outlineContainer);
     const pdf = await pdfjsLib.getDocument({
       url: this.url,
       cMapUrl: 'assets/minified/cmaps',
@@ -44,96 +41,5 @@ export class OutlineViewComponent implements OnChanges, AfterContentInit {
     });
 
     this.outline = await pdf.getOutline();
-    this.render({ outline: this.outline });
-  }
-
-  bindOutlineLink(element, { url, newWindow, dest }) {
-    const linkService = this.pdfDocumentOutline.linkService;
-
-    element.href = linkService.getDestinationHash(dest);
-    element.onclick = () => {
-      if (dest) {
-        linkService.navigateTo(dest);
-      }
-      return false;
-    };
-  }
-
-  setStyles(element, { bold, italic }) {
-    let styleStr = '';
-    if (bold) {
-      styleStr += 'font-weight: bold;';
-    }
-    if (italic) {
-      styleStr += 'font-style: italic;';
-    }
-    if (styleStr) {
-      element.setAttribute('style', styleStr);
-    }
-  }
-
-  addToggleButton(div, { count, items, }) {
-    const toggler = document.createElement('div');
-    toggler.className = 'outlineItemToggler outlineItemsHidden';
-
-    toggler.onclick = (evt) => {
-      evt.stopPropagation();
-      toggler.classList.toggle('outlineItemsHidden');
-    };
-
-    div.insertBefore(toggler, div.firstChild);
-  }
-
-  resetOutline() {
-    let firstNode = this.outlineContainer.nativeElement.firstElementChild;
-    while (firstNode) {
-      this.outlineContainer.nativeElement.removeChild(firstNode);
-      firstNode = this.outlineContainer.nativeElement.firstElementChild;
-    }
-  }
-
-  render({ outline, }) {
-    let outlineCount = 0;
-
-    if (this.outline) {
-      this.resetOutline();
-    }
-    this.outline = outline || null;
-
-    const fragment = document.createDocumentFragment();
-    const queue = [{ parent: fragment, items: this.outline, }];
-    let hasAnyNesting = false;
-    while (queue.length > 0) {
-      const levelData = queue.shift();
-      for (const item in levelData.items) {
-        const div = document.createElement('div');
-        div.className = 'outlineItem';
-
-        const element = document.createElement('a');
-        this.bindOutlineLink(element, levelData.items[item]);
-        this.setStyles(element, levelData.items[item]);
-        element.textContent = pdfjsLib.removeNullCharacters(levelData.items[item].title) || 'outline ' + outlineCount + 1;
-
-        div.appendChild(element);
-
-        if (levelData.items[item].items.length > 0) {
-          hasAnyNesting = true;
-          this.addToggleButton(div, levelData.items[item]);
-
-          const itemsDiv = document.createElement('div');
-          itemsDiv.className = 'outlineItems';
-          div.appendChild(itemsDiv);
-          queue.push({ parent: <DocumentFragment><unknown>itemsDiv, items: levelData.items[item].items, });
-        }
-        levelData.parent.appendChild(div);
-        outlineCount++;
-      }
-    }
-    if (hasAnyNesting) {
-      this.outlineContainer.nativeElement.classList.add('outlineWithDeepNesting');
-      this.pdfDocumentOutline.lastToggleIsShow =
-        (fragment.querySelectorAll('.outlineItemsHidden').length === 0);
-    }
-    this.outlineContainer.nativeElement.appendChild(fragment);
   }
 }
