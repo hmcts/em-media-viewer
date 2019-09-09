717c5f452c7bbf08f6853a6573fb7b156164b2a0
velimd <velican.duzgunce@solirius.com>
2019-09-09 13:53:43 +0100

    fixing the test for outline view

--- projects/media-viewer/src/lib/toolbar/side-bar/outline-view/outline-item/outline-item.component.spec.ts
+++ projects/media-viewer/src/lib/toolbar/side-bar/outline-view/outline-item/outline-item.component.spec.ts
@@ -1,10 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { OutlineItemComponent } from './outline-item.component';
+import { Outline } from '../outline.model';
 
 describe('OutlineItemComponent', () => {
   let component: OutlineItemComponent;
   let fixture: ComponentFixture<OutlineItemComponent>;
+
+  const outline: Outline = {
+    bold: true,
+    color: [],
+    count: 1,
+    dest: [],
+    italic: true,
+    items: [],
+    newWindow: '',
+    title: 'Outline',
+    unsafeUrl: '',
+    url: '',
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +29,22 @@ describe('OutlineItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(OutlineItemComponent);
     component = fixture.componentInstance;
+    component.outline = outline;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle outline true/false', () => {
+    component.showOutlineItems = false;
+    component.toggleOutline();
+
+    expect(component.showOutlineItems).toEqual(true);
+
+    component.toggleOutline();
+
+    expect(component.showOutlineItems).toEqual(false);
   });
 });
--- projects/media-viewer/src/lib/toolbar/side-bar/outline-view/outline-view.component.spec.ts
+++ projects/media-viewer/src/lib/toolbar/side-bar/outline-view/outline-view.component.spec.ts
@@ -1,25 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OutlineViewComponent } from './outline-view.component';
-import { PdfJsWrapperFactory } from '../../../viewers/pdf-viewer/pdf-js/pdf-js-wrapper.provider';
 import { SimpleChange } from '@angular/core';
 import * as pdfjsLib from 'pdfjs-dist';
+import { OutlineItemComponent } from './outline-item/outline-item.component';
 
 describe('OutlineViewComponent', () => {
   let component: OutlineViewComponent;
   let fixture: ComponentFixture<OutlineViewComponent>;
 
   const pdfOutline = { lastToggleIsShow: false };
-  const mockFactory = {
-    createDocumentOutline: () => pdfOutline
-  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OutlineViewComponent ],
-      providers: [
-        { provide: PdfJsWrapperFactory, useValue: mockFactory },
-      ]
+      declarations: [ OutlineViewComponent, OutlineItemComponent ],
     })
     .compileComponents();
   }));
@@ -28,6 +22,11 @@ describe('OutlineViewComponent', () => {
     fixture = TestBed.createComponent(OutlineViewComponent);
     component = fixture.componentInstance;
     component.url = 'assets/example4.pdf';
+    const mockDocument = {
+      getOutline: () => new Promise(() => {})
+    };
+    const pdf = Promise.resolve(mockDocument);
+    pdfjsLib.getDocument = () => pdf;
     fixture.detectChanges();
   });
 
@@ -38,11 +37,6 @@ describe('OutlineViewComponent', () => {
   });
 
   it('should call renderOutlineView', () => {
-    const mockDocument = {
-      getOutline: () => new Promise(() => {})
-    };
-    const pdf = Promise.resolve(mockDocument);
-    pdfjsLib.getDocument = () => pdf;
     spyOn(component, 'renderOutlineView');
 
     component.ngAfterContentInit();
@@ -51,11 +45,6 @@ describe('OutlineViewComponent', () => {
   });
 
   it('should call renderOutlineView and set the outline to empty when a new url is loaded', () => {
-    const mockDocument = {
-      getOutline: () => new Promise(() => {})
-    };
-    const pdf = Promise.resolve(mockDocument);
-    pdfjsLib.getDocument = () => pdf;
     spyOn(component, 'renderOutlineView');
 
     component.url = 'assets/example3.pdf';
@@ -66,70 +55,9 @@ describe('OutlineViewComponent', () => {
     expect(component.renderOutlineView).toHaveBeenCalled();
   });
 
-  it('should get the document outline and render it', async () => {
-    spyOn(component, 'render');
-    const mockDocument = {
-      getOutline: () => new Promise(() => {})
-    };
-    const pdf = Promise.resolve(mockDocument);
-    pdfjsLib.getDocument = () => pdf;
-    await component.renderOutlineView();
+  it('should get the document outline and render it', () => {
+    component.renderOutlineView();
 
-    expect(component.render).toHaveBeenCalled();
-  });
-
-  it('should set the styles for outline', () => {
-    const domElement = document.createElement('a');
-    const outlineItem = { bold: true, italic: true };
-
-    component.setStyles(domElement, outlineItem);
-
-    expect(domElement.getAttribute('style')).toEqual('font-weight: bold;font-style: italic;');
-  });
-
-  it('should set the toggle buttons for outlines with sub items', () => {
-    const domElement = document.createElement('div');
-    const outlineItem = { count: 1, items: [] };
-
-    component.addToggleButton(domElement, outlineItem);
-
-    const domString = '<div class="outlineItemToggler outlineItemsHidden"></div>';
-    expect(domElement.innerHTML).toEqual(domString);
-  });
-
-  it('should remove previous outline from the DOM', () => {
-    component.outlineContainer.nativeElement.innerHTML = '<div></div><div></div><div></div><div></div>';
-
-    component.resetOutline();
-
-    expect(component.outlineContainer.nativeElement.innerHTML).toEqual('');
-  });
-
-  it('should render the outline', () => {
-    spyOn(component, 'bindOutlineLink').and.callFake(() => {});
-    const outline = [
-      { bold: true, italic: true, url: 'a', newWindow: 'a', title: 'Outline', count: 1, dest: [], items: [] }];
-
-    component.render({outline : outline});
-
-    expect(component.outlineContainer.nativeElement.childElementCount).toBeGreaterThan(0);
-  });
-
-  it('should render the outline with nested outlines', async () => {
-    const mockDocument = {
-      getOutline: () => new Promise(() => {})
-    };
-    const pdf = Promise.resolve(mockDocument);
-    pdfjsLib.getDocument = () => pdf;
-
-    await component.renderOutlineView();
-
-    spyOn(component, 'bindOutlineLink').and.callFake(() => {});
-    const outlineItem1 = { bold: true, italic: true, url: 'a', newWindow: 'a', title: 'Outline item 1', count: 1, dest: [], items: [] };
-    const outlineItem2 = { bold: true, italic: true, url: 'a', newWindow: 'a', title: 'Outline item 2', count: 1, dest: [], items: [] };
-    const outline = [
-      { bold: true, italic: true, url: 'a', newWindow: 'a', title: 'Outline', count: 1, dest: [], items: [outlineItem1, outlineItem2] }];
-
-    component.render({outline : outline});
+    expect(component.outline).not.toBe([]);
   });
 });
