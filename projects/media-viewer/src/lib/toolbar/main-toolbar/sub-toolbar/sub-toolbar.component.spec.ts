ed62c9ec06b0ae2c59abe2041cdaa733accf76ea
Pawel Kaczmarek <pawel.kacmarek@solirius.com>
2019-11-15 12:31:23 +0100

    chevron submenu

--- projects/media-viewer/src/lib/toolbar/main-toolbar/sub-toolbar/sub-toolbar.component.spec.ts
+++ projects/media-viewer/src/lib/toolbar/main-toolbar/sub-toolbar/sub-toolbar.component.spec.ts
@@ -28,18 +28,62 @@ describe('SubToolbarComponent', () => {
   });
 
   it('should emit print event', () => {
+    component.toolbarButtons.showPrint = true;
+    fixture.detectChanges();
     const printSpy = spyOn(component.toolbarEvents.print, 'next');
     const printBtn = nativeElement.querySelector('button[id=secondaryPrint]');
     printBtn.click();
 
-    expect(printSpy).toHaveBeenCalledWith();
+    expect(printSpy).toHaveBeenCalledWith(true);
   });
 
   it('should emit download event', () => {
+    component.toolbarButtons.showDownload = true;
+    fixture.detectChanges();
     const downloadSpy = spyOn(component.toolbarEvents.download, 'next');
     const downloadBtn = nativeElement.querySelector('button[id=secondaryDownload]');
     downloadBtn.click();
 
-    expect(downloadSpy).toHaveBeenCalledWith();
+    expect(downloadSpy).toHaveBeenCalledWith(true);
+  });
+
+  it('should emit highlight mode event', () => {
+    component.toolbarButtons.showHighlightButton = true;
+    fixture.detectChanges();
+    const eventSpy = spyOn(component.toolbarEvents.highlightMode, 'next');
+    const button = nativeElement.querySelector('button[id=secondaryHighlightToggle]');
+    button.click();
+
+    expect(eventSpy).toHaveBeenCalledWith(true);
+  });
+
+  it('should emit draw mode event', () => {
+    component.toolbarButtons.showDrawButton = true;
+    fixture.detectChanges();
+    const eventSpy = spyOn(component.toolbarEvents.drawMode, 'next');
+    const button = nativeElement.querySelector('button[id=toggleDrawButton]');
+    button.click();
+
+    expect(eventSpy).toHaveBeenCalledWith(true);
+  });
+
+  it('should emit rotate ccw mode event', () => {
+    component.toolbarButtons.showRotate = true;
+    fixture.detectChanges();
+    const eventSpy = spyOn(component.toolbarEvents.rotate, 'next');
+    const button = nativeElement.querySelector('button[id=secondaryRotateCcw]');
+    button.click();
+
+    expect(eventSpy).toHaveBeenCalledWith(270);
+  });
+
+  it('should emit rotate cw mode event', () => {
+    component.toolbarButtons.showRotate = true;
+    fixture.detectChanges();
+    const eventSpy = spyOn(component.toolbarEvents.rotate, 'next');
+    const button = nativeElement.querySelector('button[id=secondaryRotateCw]');
+    button.click();
+
+    expect(eventSpy).toHaveBeenCalledWith(90);
   });
 });
