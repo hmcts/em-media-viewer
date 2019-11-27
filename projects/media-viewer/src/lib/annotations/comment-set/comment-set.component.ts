2170aa8543c6a578b3bf6b6dafa954d0f7100800
Tom Elliott <tomxelliott@hotmail.co.uk>
2019-11-27 17:26:15 +0000

    EM-1947: Alter approach to accomodate the image viewer

--- projects/media-viewer/src/lib/annotations/comment-set/comment-set.component.ts
+++ projects/media-viewer/src/lib/annotations/comment-set/comment-set.component.ts
@@ -203,7 +203,7 @@ export class CommentSetComponent implements OnInit, OnDestroy {
   }
 
   allCommentsSaved() {
-    this.commentService.allCommentSetsSaved();
+    this.commentService.allCommentSetsSaved(this.commentComponents);
   }
 
   allCommentsSavedInSet(): boolean {
