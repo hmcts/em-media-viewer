3be1908d5464ef563f6e976e5bc89fb0d68616dd
velimd <duzgunce.vd@gmail.com>
2020-02-20 18:26:01 +0000

    fix unit test

--- projects/media-viewer/src/lib/annotations/comment-set/comment/comment.service.spec.ts
+++ projects/media-viewer/src/lib/annotations/comment-set/comment/comment.service.spec.ts
@@ -1,18 +1,52 @@
 import { CommentService } from './comment.service';
 import { CommentSetComponent } from '../comment-set.component';
 import { Annotation } from '../../annotation-set/annotation-view/annotation.model';
-import { fakeAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AnnotationApiService } from '../../annotation-api.service';
+import { AnnotationEventService } from '../../annotation-event.service';
+import { ToolbarEventService } from '../../../toolbar/toolbar-event.service';
+import { CommentSetRenderService } from '../comment-set-render.service';
+import { annotationSet } from '../../../../assets/annotation-set';
 
 describe('CommentService', () => {
-
+  let component: CommentSetComponent;
+  let fixture: ComponentFixture<CommentSetComponent>;
   let commentService: CommentService;
+
+  const api = new AnnotationApiService({}  as any);
+  const mockAnnotationService = new AnnotationEventService();
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CommentSetComponent],
+      imports: [
+        FormsModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        { provide: AnnotationApiService, useValue: api },
+        { provide: AnnotationEventService, useValue: mockAnnotationService },
+        ToolbarEventService,
+        CommentService,
+        CommentSetRenderService
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     commentService = new CommentService();
+    fixture = TestBed.createComponent(CommentSetComponent);
+    component = fixture.componentInstance;
+    component.annotationSet = JSON.parse(JSON.stringify(annotationSet));
+    commentService.setCommentSet(component);
   });
 
-  it('should have an empty list at initialisation', () => {
-    expect(commentService.commentSetComponent).toBeNull();
+  it('should initialise comment set component', () => {
+    expect(commentService.commentSetComponent).not.toBeNull();
   });
 
   it('should update unsavedChange status to true', () => {
@@ -56,14 +90,5 @@ describe('CommentService', () => {
     } as Annotation;
 
     expect(commentService.hasUnsavedComments(annotation)).toBeFalsy();
-  });
-
-  it('onCommentChange should be called with false when commentSets is empty', () => {
-    spyOn(commentService.unsavedChanges, 'next');
-
-    commentService.allCommentsSaved();
-
-    expect(commentService.unsavedChanges.next).toHaveBeenCalledTimes(1);
-    expect(commentService.unsavedChanges.next).toHaveBeenCalledWith(false);
   });
 });
