18f579626bf781527e8807c05344e71fb53545d5
fazanki <fazanki@gmail.com>
2020-03-04 15:48:37 +0000

    extracting working version

--- projects/media-viewer/src/lib/annotations/comment-set/comment-set-header/comment-set-header.component.ts
+++ projects/media-viewer/src/lib/annotations/comment-set/comment-set-header/comment-set-header.component.ts
@@ -3,7 +3,6 @@ import {Component, EventEmitter, Input, Output, ViewEncapsulation} from '@angula
 @Component({
   selector: 'mv-comment-set-header',
   templateUrl: './comment-set-header.component.html',
-  styleUrls: ['./comment-set-header.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class CommentSetHeaderComponent {
