9907522e4a21285ab012465f700312b218173100
fazanki <fazanki@gmail.com>
2020-03-30 14:05:06 +0100

    fix to accomodate the 404 status

--- projects/media-viewer/src/lib/store/reducers/annotatons.reducer.ts
+++ projects/media-viewer/src/lib/store/reducers/annotatons.reducer.ts
@@ -78,8 +78,9 @@ export function reducer (
         loading: true
       };
     }
-    case fromAnnotations.LOAD_ANNOTATION_SET_SUCCESS: {
-      const annotationSet = !!action.payload ? action.payload : // check the 204 status no paylod
+    case fromAnnotations.LOAD_ANNOTATION_SET_SUCCESS:
+    case fromAnnotations.LOAD_ANNOTATION_SET_FAIL: {
+      const annotationSet = action.payload.status !== 404 ? action.payload :
         {
           ...state.annotationSet,
           annotations: [],
