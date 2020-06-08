72c4c1b4464582de20525f931cc6653027ff4da5
benouaer <tech@benouaer.com>
2020-06-08 22:02:08 +0100

    release v2.3.4 to fix ws issue

--- projects/media-viewer/src/lib/icp/socket.service.ts
+++ projects/media-viewer/src/lib/icp/socket.service.ts
@@ -18,7 +18,7 @@ export class SocketService implements OnDestroy {
 
   connect() {
     this.socket = socketio('/', {
-      path: '/icp/socket.io', transports: ['websocket'], secure: true
+      path: '/icp/socket.io', transports: ['websocket'], secure: false
     });
     this.socket.on('connect', () => this.connected$.next(true));
     this.socket.on('disconnect', () => this.connected$.next(false));
