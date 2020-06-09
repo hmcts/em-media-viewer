8379a791e6d8d4cbc4e669163e3f9a9b0e9c510e
fazanki <fazanki@gmail.com>
2020-06-09 12:17:33 +0100

    adding basic sorting table module

--- projects/media-viewer/src/lib/annotations/comments-summary/comments-summary.component.ts
+++ projects/media-viewer/src/lib/annotations/comments-summary/comments-summary.component.ts
@@ -1,31 +1,48 @@
-import { Component, Input, ViewChild, ElementRef, OnChanges } from '@angular/core';
+import {Component, Input, ViewChild, ElementRef, OnChanges, SimpleChanges, OnInit} from '@angular/core';
 import { PrintService } from '../../print.service';
 import { ToolbarEventService } from '../../toolbar/toolbar-event.service';
 import { AnnotationSet } from '../annotation-set/annotation-set.model';
 import { CommentsSummary } from './comments-summary.model';
 import {ViewerEventService} from '../../viewers/viewer-event.service';
+import {debug} from 'ng-packagr/lib/util/log';
+import {select, Store} from '@ngrx/store';
+import * as fromSelectors from '../../store/selectors/annotations.selectors';
+import * as fromStore from '../../store/reducers/reducers';
+import {Observable} from 'rxjs';
+import {Annotation} from '../annotation-set/annotation-view/annotation.model';
 
 @Component({
   selector: 'mv-comments-summary',
   templateUrl: './comments-summary.component.html',
 })
-export class CommentsSummaryComponent implements OnChanges {
+export class CommentsSummaryComponent implements OnChanges, OnInit {
 
   @Input() title: string;
   @Input() contentType: string;
   @Input() annotationSet: AnnotationSet | null;
-
+  public comments$: Observable<Annotation[]>;
   comments: CommentsSummary[] = [];
+  rows = [
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Molly', gender: 'Female', company: 'Burger King' }
+  ];
 
+  ColumnMode = {ColumnMode: 'flex'};
   @ViewChild('commentContainer') commentsTable: ElementRef;
 
   constructor(
+    private store: Store<fromStore.AnnotationSetState>,
     private readonly printService: PrintService,
     private readonly toolbarEvents: ToolbarEventService,
     private readonly viewerEvents: ViewerEventService
   ) {}
 
-  ngOnChanges() {
+  ngOnInit(): void {
+    this.comments$ = this.store.pipe(select(fromSelectors.getCommentsArray));
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
     if (this.annotationSet) {
       this.generateCommentsSummary();
       this.orderCommentsSummary();
