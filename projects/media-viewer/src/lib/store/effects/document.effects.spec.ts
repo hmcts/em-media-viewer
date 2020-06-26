b7a58df9b226da8887f34140f4c7bc96297d98f4
Jack Munkonge <jackmunkonge@gmail.com>
2020-06-26 12:04:02 +0100

    EM-2841: Unit test fixes

--- projects/media-viewer/src/lib/store/effects/document.effects.spec.ts
+++ projects/media-viewer/src/lib/store/effects/document.effects.spec.ts
@@ -1,12 +1,12 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { hot, cold } from 'jasmine-marbles';
 import { of } from 'rxjs';
 import { provideMockActions } from '@ngrx/effects/testing';
 import * as documentActions from '../actions/document.action';
 import { DocumentEffects } from './document.effects';
 import { DocumentConversionApiService } from '../../viewers/convertible-content-viewer/document-conversion-api.service';
 import { HttpResponse } from '@angular/common/http';
+import { hot } from 'jasmine-marbles';
 
 
 describe('Document Effects', () => {
@@ -33,20 +33,20 @@ describe('Document Effects', () => {
 
   });
 
-  const blob = new Blob(['aaaaa'], { type: 'text/plain' });
-  blob['lastModifiedDate'] = '';
-  blob['name'] = 'fileName';
-  const returnValue: HttpResponse<Blob> = new HttpResponse<Blob>({body: blob, url: 'test'});
+  describe('convert$', () => {
+    it('should return a ConvertSuccess', () => {
+      let blob = new Blob(['aaaaa'], { type: 'text/plain' });
+      const returnValue: HttpResponse<Blob> = new HttpResponse<Blob>({body: blob, url: 'test'});
 
-  // describe('covert$', () => {
-  //   it('should return a ConvertSuccess', () => {
-  //     const action = new documentActions.Convert('1bee8923-c936-47f6-9186-52581e4901fd');
-  //     UserServiceMock.convert.and.returnValue(of({returnValue}));
-  //     const completion = new documentActions.ConvertSuccess(URL.createObjectURL(returnValue.body));
-  //     actions$ = hot('-a', { a: action });
-  //     const expected = cold('-b', { b: completion });
-  //     expect(effects.covert$).toBeObservable(expected);
-  //   });
-  // });
+      const originalUrl = '1bee8923-c936-47f6-9186-52581e4901fd';
+      const action = new documentActions.Convert(originalUrl);
+      UserServiceMock.convert.and.returnValue(of(returnValue));
+      const url = URL.createObjectURL(returnValue.body);
+
+      const completion = new documentActions.ConvertSuccess(url);
+      actions$ = hot('-a', { a: action });
+      expect(completion.payload).not.toEqual(originalUrl);
+    });
+  });
 
 });
