819c121e259898372e76dbc58b3c2a693ace9ddb
Megan <meganaliceslattery@gmail.com>
2020-07-23 09:36:23 +0100

    EM-2695: File change for rebase

--- projects/media-viewer/src/lib/viewers/pdf-viewer/pdf-viewer.component.ts
+++ projects/media-viewer/src/lib/viewers/pdf-viewer/pdf-viewer.component.ts
@@ -120,6 +120,7 @@ export class PdfViewerComponent implements AfterContentInit, OnChanges, OnDestro
     this.$subscription.add(this.toolbarEvents.changePageByDeltaSubject.subscribe(pageNumber => this.pdfWrapper.changePageNumber(pageNumber)));
     this.$subscription.add(this.toolbarEvents.grabNDrag.subscribe(grabNDrag => this.enableGrabNDrag = grabNDrag));
     this.$subscription.add(this.toolbarEvents.commentsPanelVisible.subscribe(toggle => this.showCommentsPanel = toggle));
+    this.$subscription.add(this.toolbarEvents.icp.icpParticipantsListVisible.subscribe(toggle => this.showIcpParticipantsList = toggle));
     this.$subscription.add(this.pdfWrapper.positionUpdated.asObservable()
       .pipe(throttleTime(500, asyncScheduler, { leading: true, trailing: true }))
       .subscribe(event => this.store.dispatch(new PdfPositionUpdate(event.location)))
