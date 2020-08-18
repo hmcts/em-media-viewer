93065ab2a0043392aac2701721a278f4e61699df
benouaer <tech@benouaer.com>
2020-08-18 19:41:57 +0100

    fix failing tests

--- projects/media-viewer/src/lib/annotations/annotation-set/annotation-create/highlight-create.service.spec.ts
+++ projects/media-viewer/src/lib/annotations/annotation-set/annotation-create/highlight-create.service.spec.ts
@@ -1,6 +1,5 @@
 import { HighlightCreateService } from './highlight-create.service';
 import { of } from 'rxjs';
-import { fakeAsync } from '@angular/core/testing';
 
 describe('HighlightCreateService', () => {
 
@@ -34,6 +33,34 @@ describe('HighlightCreateService', () => {
     expect(saveAnnoAction.payload.rectangles).toBe(mockRectangles);
     expect(saveAnnoAction.payload.documentId).toBe('documentId');
     expect(saveAnnoAction.payload.annotationSetId).toBe('annotationSetId');
+  });
+
+  it('should apply no rotation', () => {
+    const rectangle = service
+      .applyRotation(500, 400, 200, 150, 75, 80, 0, 1);
+
+    expect(rectangle).toEqual({ x: 80, y: 75, width: 150, height: 200 });
+  });
+
+  it('should apply 90 degree rotation', () => {
+    const rectangle = service
+      .applyRotation(500, 400, 200, 150, 75, 80, 90, 1);
+
+    expect(rectangle).toEqual({ x: 75, y: 170, width: 200, height: 150 });
+  });
+
+  it('should apply 180 degree rotation', () => {
+    const rectangle = service
+      .applyRotation(500, 400, 200, 150, 75, 80, 180, 1);
+
+    expect(rectangle).toEqual({ x: 170, y: 225, width: 150, height: 200 });
+  });
+
+  it('should apply 270 degree rotation', () => {
+    const rectangle = service
+      .applyRotation(500, 400, 200, 150, 75, 80, 270, 1);
+
+    expect(rectangle).toEqual({ x: 225, y: 80, width: 200, height: 150 });
   });
 
   it('should reset highlight', () => {
