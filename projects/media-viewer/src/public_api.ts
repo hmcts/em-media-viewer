830bfd47bac2fb28d711cef92e31af2c5e0f9f79
yyaremhuk <yyaremhu@gmail.com>
2021-02-12 16:51:41 +0000

    EM-1911 Fix build issue

--- projects/media-viewer/src/public_api.ts
+++ projects/media-viewer/src/public_api.ts
@@ -4,7 +4,7 @@
 
 export * from './lib/media-viewer.module';
 export * from './lib/toolbar/toolbar.module';
-export * from './lib/annotations/annotation-api.service';
+export * from './lib/annotations/services/annotation-api/annotation-api.service';
 export * from './lib/annotations/annotations.module';
 export * from './lib/viewers/viewer-exception.model';
 export * from './lib/store/reducers/annotations.reducer';
