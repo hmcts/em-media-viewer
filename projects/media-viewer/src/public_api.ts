76b2496eb664ee4c5e3aa2df399a77af2585d226
yyaremchuk <yyaremchuk@gmail.com>
2021-03-01 16:41:20 +0000

    EM-1911 Fix build errors

--- projects/media-viewer/src/public_api.ts
+++ projects/media-viewer/src/public_api.ts
@@ -8,6 +8,6 @@ export * from './lib/annotations/services/annotation-api/annotation-api.service'
 export * from './lib/annotations/annotations.module';
 export * from './lib/viewers/viewer-exception.model';
 export * from './lib/store/reducers/annotations.reducer';
-export * from './lib/store/selectors/annotations.selectors';
-export * from './lib/store/actions/annotations.action';
+export * from './lib/store/selectors/annotation.selectors';
+export * from './lib/store/actions/annotation.actions';
 export * from './lib/store/effects/annotation.effects';
