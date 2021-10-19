7e94fb8dfc0921c93fc3e54c9dd4f41cf75fac39
Ferdi <ferdicocelli@hotmail.com>
2021-10-19 15:44:15 +0100

    crossbrowser added changes

--- e2e/saucelabs.conf.js
+++ e2e/saucelabs.conf.js
@@ -1,21 +1,40 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 const common = require('./tsconfig.e2e.json');
 const minimist = require('minimist');
 const argv = minimist(process.argv.slice(2));
+var screenShotUtils = require("protractor-screenshot-utils").ProtractorScreenShotUtils;
 
 const config = {
     framework: 'custom',
     frameworkPath: require.resolve('protractor-cucumber-framework'),
-    sauceSeleniumAddress: 'ondemand.saucelabs.com:443/wd/hub',
-    allScriptsTimeout: 111000,
-    specs: common.specFilesFilter,
-    baseUrl: (process.env.TEST_URL || 'http://localhost:3000/').replace('https', 'http'),
-    params: {
+
+    sauceSeleniumAddress: 'ondemand.eu-central-1.saucelabs.com:443/wd/hub',
+
+      host: 'ondemand.eu-central-1.saucelabs.com',
+      sauceRegion: 'eu',
+      port: 80,
+      sauceConnect: true,
+      specs: ['../features/**/*.feature'],
+
+      baseUrl: (process.env.TEST_URL || 'http://localhost:3000/').replace('https', 'http'),
+
+      params: {
         serverUrls: process.env.TEST_URL || 'http://localhost:3000/',
-        targetEnv: argv.env || 'local'
-    },
+        targetEnv: argv.env || 'local',
+        //username: process.env.TEST_EMAIL,
+        //password: process.env.TEST_PASSWORD,
+      },
+
+
+    // sauceProxy: 'http://proxyout.reform.hmcts.net:8080',  // Proxy for the REST API
     sauceUser: process.env.SAUCE_USERNAME,
     sauceKey: process.env.SAUCE_ACCESS_KEY,
+    SAUCE_REST_ENDPOINT: 'https://eu-central-1.saucelabs.com/rest/v1/',
     allScriptsTimeout: 111000,
+
+
     useAllAngular2AppRoots: true,
     multiCapabilities: [
         {
@@ -23,27 +42,33 @@ const config = {
             version: 'latest',
             platform: 'Windows 10',
             name: 'chrome-tests',
-            'tunnel-identifier': 'reformtunnel',
+            tunnelIdentifier: 'MediaViewer',
             extendedDebugging: true,
-            shardTestFiles: true,
+            shardTestFiles: false,
             maxInstances: 1
-        }
+        },
+
+            {
+              browserName: 'firefox',
+              version: 'latest',
+              platform: 'Windows 10',
+              name: 'firefox-win-tests',
+              tunnelIdentifier: 'reformtunnel',
+              extendedDebugging: true,
+              sharedTestFiles: false,
+              maxInstances: 1
+            },
     ],
     exclude: [],
+
     cucumberOpts: {
         strict: true,
-        format: ['json:cb_reports/saucelab_results.json','node_modules/cucumber-pretty'],
-        require: ['../features/step_definitions/**/*.steps.js'],
+        format: ['node_modules/cucumber-pretty', 'json:cb_reports/saucelab_results.json'],
+        require: ['../support/world.js', '../support/*.js', '../features/step_definitions/**/*.steps.js', '../features/step_definitions/**/*.steps.js'],
         tags: ['@crossbrowser']
-    },
-    onComplete() {
-        const printSessionId = function(jobName) {
-            browser.getSession()
-                .then(session => {
-                });
-        };
-        printSessionId('Media Viewer Cross Browser Tests');
-    },
+  },
+
+
     plugins: [
         {
             package: 'protractor-multiple-cucumber-html-reporter-plugin',
@@ -53,17 +78,25 @@ const config = {
                 reportName: 'Media Viewer CrossBrowser Tests',
                 jsonDir: '/reports/crossbrowser/reports',
                 reportPath: './functional-output/crossbrowser/reports'
+                displayDuration: true,
+                durationInMS: false
             }
         }
     ],
+
+
     onPrepare() {
         const caps = browser.getCapabilities();
         browser.manage()
             .window()
         browser.waitForAngularEnabled(false);
-        global.expect = common.chai.expect;
-        global.assert = common.chai.assert;
-        global.should = common.chai.should;
+        global.expect = chai.expect;
+        global.assert = chai.assert;
+        global.should = chai.should;
+
+        global.screenShotUtils = new screenShotUtils({
+              browserInstance: browser
+            });
     }
 };
 
