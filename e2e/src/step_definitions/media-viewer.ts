91cc0e72b61b6572e985d198dd030e845d455a09
matt-k1998 <matt.kolakowski@hotmail.com>
2021-10-29 16:39:45 +0100

    swap the order of the bookark scenarios, disable other scenarios, add a sleep function to media-viewer.ts

--- e2e/src/step_definitions/media-viewer.ts
+++ e2e/src/step_definitions/media-viewer.ts
@@ -280,8 +280,8 @@ When('I highlight text on a PDF document', async () => {
   await highLightTextInPdf();
 });
 
-Then('I expect no existing bookmarks present', {timeout: 2 * 5000}, async () => {
-//   await genericMethods.sleep(6000);
+Then('I expect no existing bookmarks present', async () => {
+  await genericMethods.sleep(10000);
   await clearBookmarks();
 });
 
