05a2337b60ed63f0ff67759f1726c09028c21b8e
matt-k1998 <matt.kolakowski@hotmail.com>
2021-11-01 15:01:04 +0000

    Change the timeout again

--- e2e/src/pages/app.po.ts
+++ e2e/src/pages/app.po.ts
@@ -243,7 +243,6 @@ export class AppPage {
   }
 
   async updateBookmarks(updatedText: string) {
-    await browser.sleep(1000);
     browser.sleep(1000);
     await element.all(this.bookMarkRename).first().click();
     browser.sleep(1000);
