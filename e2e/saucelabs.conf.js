ecaa83fdb63c54d71c8105d9c156804be49607ed
matt-k1998 <matt.kolakowski@hotmail.com>
2021-11-03 15:50:19 +0000

    Just run the tests on safari for macOS

--- e2e/saucelabs.conf.js
+++ e2e/saucelabs.conf.js
@@ -83,28 +83,28 @@ const config = {
 //      sharedTestFiles: false,
 //      maxInstances: 1
 //    },
-    {
-      browserName: 'firefox',
-      version: 'latest',
-      platform: 'macOS 10.15',
-      name: 'Media Viewer: macOS-latest-firefox-tests',
-      tunnelIdentifier: 'reformtunnel',
-      extendedDebugging: true,
-      capturePerformance: true,
-      sharedTestFiles: false,
-      maxInstances: 1
-    },
 //    {
-//      browserName: 'safari',
+//      browserName: 'firefox',
 //      version: 'latest',
 //      platform: 'macOS 10.15',
-//      name: 'Media Viewer: macOS-latest-safari-tests',
+//      name: 'Media Viewer: macOS-latest-firefox-tests',
 //      tunnelIdentifier: 'reformtunnel',
 //      extendedDebugging: true,
 //      capturePerformance: true,
 //      sharedTestFiles: false,
 //      maxInstances: 1
-//    }
+//    },
+    {
+      browserName: 'safari',
+      version: 'latest',
+      platform: 'macOS 10.15',
+      name: 'Media Viewer: macOS-latest-safari-tests',
+      tunnelIdentifier: 'reformtunnel',
+      extendedDebugging: true,
+      capturePerformance: true,
+      sharedTestFiles: false,
+      maxInstances: 1
+    }
 //    {
 //      browserName: 'MicrosoftEdge',
 //      version: 'latest',
