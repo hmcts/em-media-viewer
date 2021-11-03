6037dc050a8e70e9659089c9f3e84c0016c3dead
matt-k1998 <matt.kolakowski@hotmail.com>
2021-11-03 16:08:27 +0000

    Fix bookmarks scenarios on safari

--- e2e/src/step_definitions/media-viewer.ts
+++ e2e/src/step_definitions/media-viewer.ts
@@ -200,7 +200,7 @@ const updateBookmark = async function (textToBeUpdated: string) {
 
 const verifyBookmarkTextAfterUpdate = async function (textToBeUpdated: string) {
   const actualUpdatedText = await page.getUpdatedBookMarkName();
-  expect(actualUpdatedText).eq(textToBeUpdated);
+  expect(actualUpdatedText.trim().eq(textToBeUpdated.trim();
 
   await deleteBookmark();
   await verifyBookmarkCountAfterDelete();
