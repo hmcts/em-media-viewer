85095e95d02489b1c899458083c41ca99cf67881
matt-k1998 <matt.kolakowski@hotmail.com>
2021-11-04 14:32:41 +0000

    Run scenarios on Edge for MacOS

--- e2e/saucelabs.conf.js
+++ e2e/saucelabs.conf.js
@@ -61,17 +61,17 @@ const config = {
 //      sharedTestFiles: false,
 //      maxInstances: 1
 //    },
-//    {
-//      browserName: 'MicrosoftEdge',
-//      platform: 'macOS 10.15',
-//      version: '90.0',
-//      name: 'Media Viewer: macOS-latest-microsoft-edge-tests',
-//      tunnelIdentifier: 'reformtunnel',
-//      extendedDebugging: true,
-//      capturePerformance: true,
-//      sharedTestFiles: false,
-//      maxInstances: 1
-//    },
+    {
+      browserName: 'MicrosoftEdge',
+      platform: 'macOS 10.15',
+      version: '90.0',
+      name: 'Media Viewer: macOS-latest-microsoft-edge-tests',
+      tunnelIdentifier: 'reformtunnel',
+      extendedDebugging: true,
+      capturePerformance: true,
+      sharedTestFiles: false,
+      maxInstances: 1
+    },
 //    {
 //      browserName: 'chrome',
 //      version: 'latest',
@@ -105,17 +105,17 @@ const config = {
 //      sharedTestFiles: false,
 //      maxInstances: 1
 //    }
-    {
-      browserName: 'MicrosoftEdge',
-      version: 'latest',
-      platform: 'Windows 10',
-      name: 'Media Viewer: windows-latest-microsoft-edge-tests',
-      tunnelIdentifier: 'reformtunnel',
-      extendedDebugging: true,
-      capturePerformance: true,
-      sharedTestFiles: false,
-      maxInstances: 1
-    }
+//    {
+//      browserName: 'MicrosoftEdge',
+//      version: 'latest',
+//      platform: 'Windows 10',
+//      name: 'Media Viewer: windows-latest-microsoft-edge-tests',
+//      tunnelIdentifier: 'reformtunnel',
+//      extendedDebugging: true,
+//      capturePerformance: true,
+//      sharedTestFiles: false,
+//      maxInstances: 1
+//    }
   ],
 
   onPrepare: function () {
