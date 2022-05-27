7c4bd812e38f6329bded0034da4af6ed3101c8f9
matt-k1998 <matt.kolakowski@hotmail.com>
2022-05-27 10:12:13 +0100

    More updates, bump MV version, fix errors

--- projects/media-viewer/src/public_api.ts
+++ projects/media-viewer/src/public_api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of media-viewer
  */
 
+import { from } from 'rxjs';
+
 export * from './lib/media-viewer.module';
 export * from './lib/toolbar/toolbar.module';
 export * from './lib/annotations/services/annotation-api/annotation-api.service';
@@ -11,3 +13,21 @@ export * from './lib/store/reducers/annotations.reducer';
 export * from './lib/store/selectors/annotation.selectors';
 export * from './lib/store/actions/annotation.actions';
 export * from './lib/store/effects/annotation.effects';
+export * from './lib/media-viewer.component';
+export * from './lib/toolbar/main-toolbar/main-toolbar.component';
+export * from './lib/toolbar/search-bar/search-bar.component';
+export * from './lib/toolbar/redaction-toolbar/redaction-toolbar.component';
+export * from './lib/toolbar/icp-toolbar/icp-toolbar.component';
+export * from './lib/annotations/annotation-set/annotation-view/annotation-view.component'
+export * from './lib/annotations/annotation-set/annotation-create/box-highlight-create/box-highlight-create.component'
+export * from './lib/annotations/annotation-set/annotation-set.component'
+export * from './lib/annotations/comment-set/comment-set.component'
+export * from './lib/annotations/comment-set/comment-set-header/comment-set-header.component'
+export * from './lib/annotations/comment-set/comment-set-header/comment-search/comment-search.component'
+export * from './lib/annotations/comments-summary/comments-summary.component'
+export * from './lib/annotations/tags/tags.component'
+export * from './lib/annotations/comment-set/comment-set-header/comment-filter/comment-filter.component'
+export * from './lib/annotations/annotation-set/metadata-layer/metadata-layer.component'
+export * from './lib/annotations/pipes/filter/filter.pipe'
+export * from './lib/annotations/pipes/unsnake/unsnake.pipe'
+export * from './lib/annotations/pipes/date/date.pipe'
