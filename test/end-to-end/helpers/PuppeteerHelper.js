7ace6af07f329e0119aa24ad3f497b770dbb3ec2
PhaniP <phani.perla@hmcts.net>
2022-08-16 10:01:50 +0100

    EM-4524-Puppeteer Helper

---
+++ test/end-to-end/helpers/PuppeteerHelper.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const Helper = codecept_helper;
+const helperName = 'Puppeteer';
+const testConfig = require('../../config');
+
+class PuppeteerHelper extends Helper {
+
+  async waitForNavigationToComplete(locator) {
+    const page = this.helpers[helperName].page;
+    const promises = [];
+
+    promises.push(page.waitForNavigation({timeout: 1200000, waitUntil: ['domcontentloaded']}));
+
+    if (locator) {
+      promises.push(page.click(locator));
+    }
+    await Promise.all(promises);
+  }
+
+  async clickTab(tabTitle) {
+    const helper = this.helpers[helperName];
+    if (testConfig.TestForXUI) {
+      const tabXPath = `//div[text()='${tabTitle}']`;
+
+      await helper.page.waitForXPath(tabXPath);
+      const clickableTab = await helper.page.$x(tabXPath);
+      await helper.page.evaluate(el => el.click(), clickableTab[0]);
+    } else {
+      helper.click(tabTitle);
+    }
+  }
+}
+
+module.exports = PuppeteerHelper;
