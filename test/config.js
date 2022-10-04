3d2eaeafa2ddbbbf07d7f63c2bee7c4f6539170d
PhaniP <phani.perla@hmcts.net>
2022-10-04 16:41:43 +0100

    update

--- test/config.js
+++ test/config.js
@@ -13,7 +13,7 @@ module.exports = {
   TestEnvCWPassword: process.env.CCD_CASEWORKER_E2E_PASSWORD || '',
   TestForXUI: process.env.TESTS_FOR_XUI_SERVICE === 'true',
   TestForAccessibility: process.env.TESTS_FOR_ACCESSIBILITY === 'true',
-  TestForCrossBrowser: process.env.TESTS_FOR_CROSS_BROWSER === 'false',
+  TestForCrossBrowser: process.env.TESTS_FOR_CROSS_BROWSER === 'true',
   TestIdamClientSecret: process.env.IDAM_CLIENT_SECRET || '',
   TestS2SAuthSecret: process.env.SERVICE_SECRET || '',
 };
