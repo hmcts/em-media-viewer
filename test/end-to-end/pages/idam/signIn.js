9741c8daf0bc46df7ab11103ea4fdc4519c0a5f7
PhaniP <phani.perla@hmcts.net>
2022-10-13 10:27:56 +0100

    EM-4659-MV-CodeceptJS-SmokeTest

---
+++ test/end-to-end/pages/idam/signIn.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const testConfig = require('../../../config');
+
+module.exports = async function () {
+
+  const I = this;
+  await I.amOnPage('/', 60);
+  await I.waitForText('Sign in');
+  await I.fillField('username', testConfig.TestEnvCWUser);
+  await I.fillField('password', testConfig.TestEnvCWPassword);
+  await I.click('input[value="Sign in"]');
+  await I.wait(10);
+  await I.waitForText('Case list', testConfig.TestTimeToWaitForText);
+};
