9f99e7afb7fdb33b09e3edbfaf1d965133417b10
PhaniP <phani.perla@hmcts.net>
2022-10-13 17:01:08 +0100

    update

--- test/end-to-end/pages/idam/signIn.js
+++ test/end-to-end/pages/idam/signIn.js
@@ -5,9 +5,10 @@ const testConfig = require('../../../config');
 module.exports = async function () {
 
   const I = this;
-  await I.amOnPage('/', 60);
+  await I.amOnPage('/', testConfig.TestTimeToWaitForText);
   await I.waitForText('Sign in');
   await I.fillField('username', testConfig.TestEnvCWUser);
   await I.fillField('password', testConfig.TestEnvCWPassword);
   await I.click('input[value="Sign in"]');
+  await I.waitForText('Case list', testConfig.TestTimeToWaitForText);
 };
