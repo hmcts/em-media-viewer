917699671dfa58404bbd59ffd86357651773d424
PhaniP <phani.perla@hmcts.net>
2022-10-19 17:28:29 +0100

    EM-4584-Upload Document E2E

---
+++ test/end-to-end/pages/common/constants.js
@@ -0,0 +1,14 @@
+const eventNames = {
+  ACCEPT_CASE: 'Accept/Reject Case',
+  UPLOAD_DOCUMENT: 'Upload Document'
+};
+
+const states = {
+  SUBMITTED: 'Submitted',
+  ACCEPTED: 'Accepted'
+};
+
+module.exports = {
+  eventNames,
+  states
+};
--- test/end-to-end/pages/steps.js
+++ test/end-to-end/pages/steps.js
@@ -4,7 +4,9 @@ const requireDirectory = require('require-directory');
 const steps = requireDirectory(module);
 
 module.exports = () => {
-    return actor({
-        authenticateWithIdam: steps.idam.signIn
-    });
+  return actor({
+    authenticateWithIdam: steps.idam.signIn,
+    chooseNextStep: steps.nextStep.nextStep,
+    executeUploadDocument: steps.uploadDocument.uploadDocument,
+  });
 };
