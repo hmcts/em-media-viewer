5cae9cbeb9264778192a5529e64d3a4c2282551e
PhaniP <phani.perla@hmcts.net>
2022-10-21 16:19:06 +0100

    EM-4685-Create A CCD Case

--- test/end-to-end/pages/steps.js
+++ test/end-to-end/pages/steps.js
@@ -4,7 +4,8 @@ const requireDirectory = require('require-directory');
 const steps = requireDirectory(module);
 
 module.exports = () => {
-    return actor({
-        authenticateWithIdam: steps.idam.signIn
-    });
+  return actor({
+    authenticateWithIdam: steps.idam.signIn,
+    chooseNextStep: steps.nextStep.nextStep
+  });
 };
