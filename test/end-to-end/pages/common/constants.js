52d719722271a9a44aecc173ee5eb771dc9bf88e
PhaniP <phani.perla@hmcts.net>
2022-10-26 15:57:34 +0100

    EM-4584-EM-4707-EM-4710-EM-4711-Document upload DM Store Scenarios

---
+++ test/end-to-end/pages/common/constants.js
@@ -0,0 +1,13 @@
+const ccdEvents = {
+  UPLOAD_DOCUMENT: 'Upload Document'
+};
+
+const states = {
+  SUBMITTED: 'Submitted',
+  ACCEPTED: 'Accepted'
+};
+
+module.exports = {
+  ccdEvents,
+  states
+};
