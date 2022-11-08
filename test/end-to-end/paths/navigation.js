751856763c2659c2f0419d77c18ed67e92582af1
PhaniP <phani.perla@hmcts.net>
2022-11-08 16:52:35 +0000

    Merged Zoom & Navigation Sceanrios in single featue

--- test/end-to-end/paths/navigation.js
+++ test/end-to-end/paths/navigation.js
@@ -1,10 +1,11 @@
 const testConfig = require('./../../config');
 const {createCaseInCcd} = require("../helpers/ccdDataStoreApi");
-const {ccdEvents} = require('../pages/common/constants.js');
+const {pdfViewerZoomInOut} = require("../helpers/mvCaseHelper");
+const {ccdEvents, mvData} = require('../pages/common/constants.js');
 const {pdfViewerPageNavigationTest} = require("../helpers/mvCaseHelper");
 let caseId;
 
-Feature('Navigation Feature');
+Feature('Navigation & Zoom Feature Scenarios');
 
 BeforeSuite(async ({I}) => caseId = await createCaseInCcd('test/end-to-end/data/ccd-case-basic-data.json'));
 
@@ -22,3 +23,16 @@ Scenario('Enable page navigation with in a file', async ({I}) => {
 }).tag('@xb')
   .tag('@nightly')
   .retry(testConfig.TestRetryScenarios);
+
+Scenario('PDF Viewer Zoom In/Out', async ({I}) => {
+  await pdfViewerZoomInOut(I, caseId, ccdEvents.UPLOAD_DOCUMENT, mvData.PDF_DOCUMENT);
+
+}).tag('@ci')
+  .tag('@nightly')
+  .retry(testConfig.TestRetryScenarios);
+
+Scenario('Image Viewer Zoom In/Out', async ({I}) => {
+  await pdfViewerZoomInOut(I, caseId, ccdEvents.UPLOAD_DOCUMENT, mvData.IMAGE_DOCUMENT);
+
+}).tag('@nightly')
+  .retry(testConfig.TestRetryScenarios)
