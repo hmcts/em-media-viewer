b9c50ff545d5d3579186f26edeee322fc8ed2744
phani-perla1 <87771652+phani-perla1@users.noreply.github.com>
2022-11-09 18:19:29 +0000

    [EM-4538]-Migrate MV Download Tests (#592)
    * EM-4538-Migrate Download and Print Scenarios to CodeceptJS
    git add .
    
    * update
    
    Co-authored-by: PhaniP <phani.perla@hmcts.net>

--- test/end-to-end/pages/steps.js
+++ test/end-to-end/pages/steps.js
@@ -4,17 +4,19 @@ const requireDirectory = require('require-directory');
 const steps = requireDirectory(module);
 
 module.exports = () => {
-  return actor({
-    authenticateWithIdam: steps.idam.signIn,
-    chooseNextStep: steps.nextStep.nextStep,
-    uploadPdfDoc: steps.uploadDocument.uploadPdfDocument,
-    uploadImage: steps.uploadDocument.uploadImageJpeg,
-    uploadWordDoc: steps.uploadDocument.uploadWordDocument,
-    executeContentSearch: steps.search.contentSearch,
-    searchResultsNavigationUsingPreviousAndNextLinks: steps.search.searchResultsNavigationUsingNextAndPrevious,
-    enterShouldJumpViewerToNextSearchResult: steps.search.enterShouldJumpViewerToNextSearchResults,
-    executePdfViewerZoom: steps.zoom.pdfViewerZoom,
-    openPdfInMediaViewer: steps.commonSteps.viewPdfDocInMediaViewer,
-    pdfViewerPageNavigation:steps.navigationMV.pdfViewerNavigation
-  });
+    return actor({
+        authenticateWithIdam: steps.idam.signIn,
+        chooseNextStep: steps.nextStep.nextStep,
+        uploadPdfDoc: steps.uploadDocument.uploadPdfDocument,
+        uploadImage: steps.uploadDocument.uploadImageJpeg,
+        uploadWordDoc: steps.uploadDocument.uploadWordDocument,
+        executeContentSearch: steps.search.contentSearch,
+        searchResultsNavigationUsingPreviousAndNextLinks: steps.search.searchResultsNavigationUsingNextAndPrevious,
+        enterShouldJumpViewerToNextSearchResult: steps.search.enterShouldJumpViewerToNextSearchResults,
+        executePdfViewerZoom: steps.zoom.pdfViewerZoom,
+        openPdfInMediaViewer: steps.commonSteps.viewPdfDocInMediaViewer,
+        pdfViewerPageNavigation: steps.navigationMV.pdfViewerNavigation,
+        downloadPdfDocument: steps.downloadAndPrint.mvDownload,
+        MvPrintDocument: steps.downloadAndPrint.mvPrint,
+    });
 };
