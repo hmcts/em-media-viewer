78cd1be04598a8615e2f7930e40a12360fb428f1
PhaniP <phani.perla@hmcts.net>
2022-11-14 11:20:41 +0000

    update

--- test/end-to-end/mvFeatures/pdfAndImageRotation.js
+++ test/end-to-end/mvFeatures/pdfAndImageRotation.js
@@ -2,16 +2,16 @@ const testConfig = require('./../../config');
 const {mvData} = require('../pages/common/constants.js');
 const {pdfAndImageRotationTest} = require("../helpers/mvCaseHelper");
 
-Feature('Pdf & Image Rotation');
+Feature('Pdf & Image Rotation Feature');
 
-Scenario('PDF Viewer Zoom In/Out', async ({I}) => {
+Scenario('Rotate Pdf viewer document', async ({I}) => {
   await pdfAndImageRotationTest(I, mvData.CASE_ID, mvData.PDF_DOCUMENT);
 
 }).tag('@ci')
   .tag('@nightly')
   .retry(testConfig.TestRetryScenarios);
 
-Scenario('Image Viewer Zoom In/Out', async ({I}) => {
+Scenario('Rotate Image viewer document', async ({I}) => {
   await pdfAndImageRotationTest(I, mvData.CASE_ID, mvData.IMAGE_DOCUMENT);
 
 }).tag('@nightly')
