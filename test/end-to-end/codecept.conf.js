f86ebb6d8634bc944245eb634bac095b7a163976
PhaniP <phani.perla@hmcts.net>
2022-11-23 14:59:19 +0000

    update

--- test/end-to-end/codecept.conf.js
+++ test/end-to-end/codecept.conf.js
@@ -2,93 +2,91 @@ const config = require('../config');
 const {createCaseInCcd} = require("./helpers/ccdDataStoreApi");
 
 exports.config = {
-  tests: config.TestPathToRun,
-  output: `${process.cwd()}/${config.TestOutputDir}`,
-
-  async bootstrap() {
-    config.CCDCaseID = await createCaseInCcd('test/end-to-end/data/ccd-case-basic-data.json');
-    console.log('Case ID==>::' + config.CCDCaseID);
-  },
-  async teardown() {
-    console.log(" I am done");
-  },
-  helpers: {
-    Puppeteer: {
-      url: config.TestUrl,
-      waitForTimeout: 40000,
-      getPageTimeout: 40000,
-      waitForAction: 1000,
-      show: config.TestShowBrowserWindow,
-      waitForNavigation: ['domcontentloaded'],
-      restart: true,
-      keepCookies: false,
-      keepBrowserState: false,
-      chrome: {
-        ignoreHTTPSErrors: true,
-        'ignore-certificate-errors': true,
-        'defaultViewport': {
-          'width': 1280,
-          'height': 960
+    tests: config.TestPathToRun,
+    output: `${process.cwd()}/${config.TestOutputDir}`,
+    async bootstrapAll() {
+      config.CCDCaseID = await createCaseInCcd('test/end-to-end/data/ccd-case-basic-data.json');
+      console.log("Case Id==>::" + config.CCDCaseID);
+    },
+    async teardownAll() {
+    },
+    helpers: {
+        Puppeteer: {
+            url: config.TestUrl,
+            waitForTimeout: 40000,
+            getPageTimeout: 40000,
+            waitForAction: 1000,
+            show: config.TestShowBrowserWindow,
+            waitForNavigation: ['domcontentloaded'],
+            restart: true,
+            keepCookies: false,
+            keepBrowserState: false,
+            chrome: {
+                ignoreHTTPSErrors: true,
+                'ignore-certificate-errors': true,
+                'defaultViewport': {
+                    'width': 1280,
+                    'height': 960
+                },
+                args: [
+                    '--headless',
+                    '--disable-gpu',
+                    '--no-sandbox',
+                    '--allow-running-insecure-content',
+                    '--ignore-certificate-errors',
+                    '--window-size=1440,1400'
+                ]
+            },
         },
-        args: [
-          '--headless',
-          '--disable-gpu',
-          '--no-sandbox',
-          '--allow-running-insecure-content',
-          '--ignore-certificate-errors',
-          '--window-size=1440,1400'
-        ]
-      },
+        PuppeteerHelper: {
+            require: './helpers/PuppeteerHelper.js'
+        },
+      REST: {},
+      JSONResponse: {},
+        JSWait: {require: './helpers/JSWait.js'},
     },
-    PuppeteerHelper: {
-      require: './helpers/PuppeteerHelper.js'
+    include: {
+        I: './pages/steps.js'
     },
-    REST: {},
-    JSONResponse: {},
-    JSWait: {require: './helpers/JSWait.js'},
-  },
-  include: {
-    I: './pages/steps.js'
-  },
-  plugins: {
-    screenshotOnFail: {
-      enabled: true,
-      fullPageScreenshots: true
-    },
-    retryFailedStep: {
-      enabled: true,
-      retries: 1
-    },
-    autoDelay: {
-      enabled: true
-    }
-  },
-  mocha: {
-    reporterOptions: {
-      'codeceptjs-cli-reporter': {
-        stdout: '-',
-        options: {steps: true}
-      },
-      'mocha-junit-reporter': {
-        stdout: '-',
-        options: {mochaFile: './functional-output/result.xml'}
-      },
-      mochawesome: {
-        stdout: './functional-output/mv-e2e-mochawesome-stdout.log',
-        options: {
-          reportDir: config.TestOutputDir || './functional-output',
-          reportFilename: 'mv-e2e-result',
-          inlineAssets: true,
-          reportTitle: 'MV E2E Tests'
+    plugins: {
+        screenshotOnFail: {
+            enabled: true,
+            fullPageScreenshots: true
+        },
+        retryFailedStep: {
+            enabled: true,
+            retries: 1
+        },
+        autoDelay: {
+            enabled: true
         }
-      }
-    }
-  },
-  multiple: {
-    parallel: {
-      chunks: 2,
-      browsers: ['chrome']
-    }
-  },
-  'name': 'mv-e2e-tests'
+    },
+    mocha: {
+        reporterOptions: {
+            'codeceptjs-cli-reporter': {
+                stdout: '-',
+                options: {steps: true}
+            },
+            'mocha-junit-reporter': {
+                stdout: '-',
+                options: {mochaFile: './functional-output/result.xml'}
+            },
+            mochawesome: {
+                stdout: './functional-output/mv-e2e-mochawesome-stdout.log',
+                options: {
+                    reportDir: config.TestOutputDir || './functional-output',
+                    reportFilename: 'mv-e2e-result',
+                    inlineAssets: true,
+                    reportTitle: 'MV E2E Tests'
+                }
+            }
+        }
+    },
+    multiple: {
+        parallel: {
+            chunks: 2,
+            browsers: ['chrome']
+        }
+    },
+    'name': 'mv-e2e-tests'
 };
