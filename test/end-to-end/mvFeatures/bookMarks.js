c370e5ee6040fa4ceb8654421a926cbbd230d251
phani-perla1 <87771652+phani-perla1@users.noreply.github.com>
2022-11-29 10:15:19 +0000

    EM-4842-Ability To Highlight Text And Drawbox (#606)
    Co-authored-by: PhaniP <phani.perla@hmcts.net>

--- test/end-to-end/mvFeatures/bookMarks.js
+++ test/end-to-end/mvFeatures/bookMarks.js
@@ -6,28 +6,28 @@ const {mvData} = require('../pages/common/constants.js');
 Feature('Bookmarks Feature');
 
 Scenario('Create a bookmark using highlight function', async ({I}) => {
-  await createBookmarkTest(I, mvData.CCD_CASE_FOR_BOOKMARKS, mvData.PDF_DOCUMENT);
+  await createBookmarkTest(I, mvData.ANNOTATIONS_BOOKMARKS_CASE, mvData.PDF_DOCUMENT);
 
 }).tag('@ci')
   .tag('@np')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Delete Bookmark', async ({I}) => {
-  await deleteBookmarkTest(I, mvData.CCD_CASE_FOR_BOOKMARKS, mvData.PDF_DOCUMENT);
+  await deleteBookmarkTest(I, mvData.ANNOTATIONS_BOOKMARKS_CASE, mvData.PDF_DOCUMENT);
 
 }).tag('@ci')
   .tag('@np')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Update Bookmark', async ({I}) => {
-  await updateBookmarkTest(I, mvData.CCD_CASE_FOR_BOOKMARKS, mvData.PDF_DOCUMENT);
+  await updateBookmarkTest(I, mvData.ANNOTATIONS_BOOKMARKS_CASE, mvData.PDF_DOCUMENT);
 
 }).tag('@ci')
   .tag('@np')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Add an empty bookmark', async ({I}) => {
-  await addEmptyBookmarksTest(I, mvData.CCD_CASE_FOR_BOOKMARKS, mvData.PDF_DOCUMENT);
+  await addEmptyBookmarksTest(I, mvData.ANNOTATIONS_BOOKMARKS_CASE, mvData.PDF_DOCUMENT);
 
 }).tag('@np')
   .retry(testConfig.TestRetryScenarios);
--- test/end-to-end/pages/pdfViewerAnnotations/clearExistingTextHighlights.js
+++ test/end-to-end/pages/pdfViewerAnnotations/clearExistingTextHighlights.js
@@ -1,14 +1,15 @@
 'use strict'
+
 const commonConfig = require('../../data/commonConfig.json');
 const testConfig = require("../../../config");
 
 module.exports = async function () {
   const I = this;
-  await I.click(commonConfig.mvHighLight);
+  await I.wait(testConfig.BookmarksAndAnnotationsWait);
 
   await I.executeScript(async () => {
     const range = document.createRange();
-    const matchingElement = document.getElementsByClassName('textLayer')[0].children[3];
+    const matchingElement = document.getElementsByClassName('textLayer')[0].children[0];
     range.selectNodeContents(matchingElement);
     const sel = window.getSelection();
     sel.removeAllRanges();
@@ -16,10 +17,12 @@ module.exports = async function () {
 
     const mouseUpEvent = document.createEvent('MouseEvents');
     mouseUpEvent.initMouseEvent('mouseup', true, true, window, 1, 844, 497, 937, 403, false, false, false, false, 0, null);
-    const pageHandle = document.getElementsByClassName('textLayer')[0].children[3];
+    const pageHandle = document.getElementsByClassName('textLayer')[0].children[0];
     pageHandle.dispatchEvent(mouseUpEvent);
   });
 
-  await I.wait(testConfig.BookmarksAndAnnotationsWait);
-  await I.click(commonConfig.bookMarksButton);
+  while (await I.getBookmarksCount(commonConfig.highLightTextCount) !== 0) {
+    await I.click(commonConfig.commentPopup.replace('Comment', 'Delete'));
+    await I.wait(testConfig.BookmarksAndAnnotationsWait);
+  }
 }
