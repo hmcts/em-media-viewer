9f1eb51ad7b7ef8a720377a2f1e0abdf47d0aada
PhaniP <phani.perla@hmcts.net>
2022-12-06 10:11:33 +0000

    EM-4861-Update A Comment Test

--- test/end-to-end/pages/annotationsAndComments/highlightPdfText.js
+++ test/end-to-end/pages/annotationsAndComments/highlightPdfText.js
@@ -3,26 +3,24 @@ const commonConfig = require('../../data/commonConfig.json');
 const testConfig = require("../../../config");
 
 module.exports = async function () {
-
   const I = this;
   await I.click(commonConfig.mvHighLight);
 
   await I.executeScript(async () => {
     const range = document.createRange();
-    const matchingElement = document.getElementsByClassName('textLayer')[0].children[0];
+    const matchingElement = document.evaluate("//span[text()='Apelydd']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    // const matchingElement = document.getElementsByClassName('textLayer')[0].children[0];
     range.selectNodeContents(matchingElement);
     const sel = window.getSelection();
     sel.removeAllRanges();
     sel.addRange(range);
 
     const mouseUpEvent = document.createEvent('MouseEvents');
-    mouseUpEvent.initMouseEvent('mouseup', true, true, window, 1, 844, 497, 937, 403, false, false, false, false, 0, null);
-    const pageHandle = document.getElementsByClassName('textLayer')[0].children[0];
+    mouseUpEvent.initEvent('mouseup', true, true)
+    // mouseUpEvent.initMouseEvent('mouseup', true, true, window, 1, 844, 497, 937, 403, false, false, false, false, 0, null);
+    const pageHandle = document.getElementsByClassName('textLayer')[0].children[10];
     pageHandle.dispatchEvent(mouseUpEvent);
   });
 
-  await I.retry(2).click(commonConfig.highLightPopup)
-  await I.click('//span[text()=\'Apelydd\']');
-  const screenShot = Date.now() + 'Annotations-highlight' + '.png';
-  await I.saveScreenshot(screenShot, true);
+  await I.wait(testConfig.BookmarksAndAnnotationsWait);
 }
