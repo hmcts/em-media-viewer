df5aa4b35afb41a2e9dc90ede95c5748ceca13a1
PhaniP <phani.perla@hmcts.net>
2023-01-03 16:26:33 +0000

    EM-4925-Annotations And Comments Tests Execution In Preview Env

--- test/end-to-end/pages/annotationsAndComments/highlightPdfText.js
+++ test/end-to-end/pages/annotationsAndComments/highlightPdfText.js
@@ -8,17 +8,17 @@ module.exports = async function () {
 
   await I.executeScript(async () => {
     const range = document.createRange();
-    const matchingElement = document.evaluate("//span[text()='Apelydd']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    // const matchingElement = document.getElementsByClassName('textLayer')[0].children[0];
+    // const matchingElement = document.evaluate("//span[text()='Apelydd']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    const matchingElement = document.getElementsByClassName('textLayer')[0].children[4];
     range.selectNodeContents(matchingElement);
     const sel = window.getSelection();
     sel.removeAllRanges();
     sel.addRange(range);
 
     const mouseUpEvent = document.createEvent('MouseEvents');
-    mouseUpEvent.initEvent('mouseup', true, true)
-    // mouseUpEvent.initMouseEvent('mouseup', true, true, window, 1, 844, 497, 937, 403, false, false, false, false, 0, null);
-    const pageHandle = document.getElementsByClassName('textLayer')[0].children[10];
+    // mouseUpEvent.initEvent('mouseup', true, true)
+    mouseUpEvent.initMouseEvent('mouseup', true, true, window, 1, 844, 497, 937, 403, false, false, false, false, 0, null);
+    const pageHandle = document.getElementsByClassName('textLayer')[0].children[4];
     pageHandle.dispatchEvent(mouseUpEvent);
   });
 
