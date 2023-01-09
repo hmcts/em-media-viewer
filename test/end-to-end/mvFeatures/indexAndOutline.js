78d566b9d0b5d52c01f5767dc8dcb83fbc24f64f
MichaelJacob21012 <88201349+MichaelJacob21012@users.noreply.github.com>
2023-01-09 17:27:14 +0100

    mark test tag as ci

--- test/end-to-end/mvFeatures/indexAndOutline.js
+++ test/end-to-end/mvFeatures/indexAndOutline.js
@@ -7,11 +7,11 @@ Feature('Index & Outline Feature');
 Scenario('Navigate Bundle Documents Through Page Index Number', async ({I}) => {
   await navigateBundleDocsUsingPageIndexTest(I, mvData.INDEX_AND_OUTLINE_CASE, mvData.PDF_DOCUMENT, mvData.BUNDLE_DOCUMENT_NAME_TO_NAVIGATE, mvData.OUTLINE_PAGE_NUMBER_TO_NAVIGATE, mvData.ASSERT_BUNDLE_DOC_NAME);
 
-}).tag('@EM-4865')
+}).tag('@ci')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Navigate Nested Documents Using Index', async ({I}) => {
   await navigateNestedDocsUsingIndexTest(I, mvData.INDEX_AND_OUTLINE_CASE, mvData.PDF_DOCUMENT, mvData.NESTED_DOCUMENT_NAME_TO_NAVIGATE, mvData.NESTED_PAGE_NUMBER_TO_NAVIGATE, mvData.NESTED_PAGE_CONTENT);
 
-}).tag('@EM-4865')
+}).tag('@ci')
   .retry(testConfig.TestRetryScenarios);
