f63b02e20948d307e83c1094f2b7790eb3b00140
Munish Sharma <Munish.Sharma@HMCTS.NET>
2023-01-10 14:34:37 +0000

    changes based on sonar

--- projects/media-viewer/src/lib/icp/icp-update.service.ts
+++ projects/media-viewer/src/lib/icp/icp-update.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { SocketService } from './socket.service';
 import { IcpParticipant, IcpScreenUpdate, IcpSession } from './icp.interfaces';
-import { from, of } from 'rxjs';
 import { IcpEvents } from './icp.events';
 
 @Injectable()
