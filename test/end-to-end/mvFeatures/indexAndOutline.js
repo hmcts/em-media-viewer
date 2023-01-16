830752e42d89eb1d20fb190de201484071a15f80
PhaniP <phani.perla@hmcts.net>
2023-01-16 10:13:19 +0000

    EM-4962-Media Viewer Functional Tests Fix

--- test/end-to-end/mvFeatures/indexAndOutline.js
+++ test/end-to-end/mvFeatures/indexAndOutline.js
@@ -7,11 +7,11 @@ Feature('Index & Outline Feature');
 Scenario('Navigate Bundle Documents Through Page Index Number', async ({I}) => {
   await navigateBundleDocsUsingPageIndexTest(I, mvData.PREVIEW_TEST_DATA, mvData.PDF_DOCUMENT, mvData.BUNDLE_DOCUMENT_NAME_TO_NAVIGATE, mvData.OUTLINE_PAGE_NUMBER_TO_NAVIGATE, mvData.ASSERT_BUNDLE_DOC_NAME);
 
-}).tag('@ci')
+}).tag('@fnd')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Navigate Nested Documents Using Index', async ({I}) => {
   await navigateNestedDocsUsingIndexTest(I, mvData.PREVIEW_TEST_DATA, mvData.PDF_DOCUMENT, mvData.NESTED_DOCUMENT_NAME_TO_NAVIGATE, mvData.NESTED_PAGE_NUMBER_TO_NAVIGATE, mvData.NESTED_PAGE_CONTENT);
 
-}).tag('@ci')
+}).tag('@fnd')
   .retry(testConfig.TestRetryScenarios);
