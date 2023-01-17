41ce851e8999558e62eacdec76a051613a5b1c9e
PhaniP <phani.perla@hmcts.net>
2023-01-17 14:02:27 +0000

    update

--- test/end-to-end/mvFeatures/redact.js
+++ test/end-to-end/mvFeatures/redact.js
@@ -15,37 +15,37 @@ Feature('Redact Feature');
 Scenario('Mark Content For Redaction Using Draw Box Function ', async ({I}) => {
   await markContentForRedactionUsingDrawBoxTest(I, mvData.REDACTION_CASE, mvData.PDF_DOCUMENT);
 
-}).tag('@ignore')
+}).tag('@ci')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Redact Content Using Redact Text Function', async ({I}) => {
   await redactContentUsingRedactTextTest(I, mvData.REDACTION_CASE, mvData.PDF_DOCUMENT);
 
-}).tag('@ignore')
+}).tag('@ci')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Create Redactions Using Draw Box and Redact Text Functions', async ({I}) => {
   await createRedactionsUsingDrawBoxAndRedactText(I, mvData.REDACTION_CASE, mvData.PDF_DOCUMENT);
 
-}).tag('@ignore')
+}).tag('@ci')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Preview all content marked for redaction', async ({I}) => {
   await previewAllRedactionsTest(I, mvData.REDACTION_CASE, mvData.PDF_DOCUMENT);
 
-}).tag('@ignore')
+}).tag('@ci')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Save redactions to download', async ({I}) => {
   await saveAllRedactionsTest(I, mvData.REDACTION_CASE, mvData.PDF_DOCUMENT);
 
-}).tag('@ignore')
+}).tag('@ci')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Redact text and then removing the redaction', async ({I}) => {
   await redactTextAndThenRemovingRedactionTest(I, mvData.REDACTION_CASE, mvData.PDF_DOCUMENT);
 
-}).tag('@ignore')
+}).tag('@ci')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Unmark selected content (marked for redaction)', async ({I}) => {
@@ -60,7 +60,7 @@ Scenario('Unmark all content (marked for redaction)', async ({I}) => {
 }).tag('@wip')
   .retry(testConfig.TestRetryScenarios);
 
-Scenario(' Redact Scenarios ==>:: 1.' +
+Scenario(' Redact Scenarios ==>::' +
   '1.Mark Content For Redaction Using Draw Box Function' +
   '2.Redact Content Using Redact Text Function' +
   '3. Create Redactions Using Draw Box and Redact Text Functions' +
@@ -70,5 +70,5 @@ Scenario(' Redact Scenarios ==>:: 1.' +
   '7.Done', async ({I}) => {
   await executeAllRedactScenarios(I, mvData.REDACTION_CASE, mvData.PDF_DOCUMENT);
 
-}).tag('@ci')
+}).tag('@pr')
   .retry(testConfig.TestRetryScenarios)
