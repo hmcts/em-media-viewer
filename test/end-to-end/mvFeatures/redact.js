32f063f3dd16f9d38c6083f706175e4f4598d06d
PhaniP <phani.perla@hmcts.net>
2023-01-17 13:56:56 +0000

    EM-4970-Media Viewer Test Execution Performance

--- test/end-to-end/mvFeatures/redact.js
+++ test/end-to-end/mvFeatures/redact.js
@@ -5,7 +5,8 @@ const {
   redactTextAndThenRemovingRedactionTest,
   createRedactionsUsingDrawBoxAndRedactText,
   previewAllRedactionsTest,
-  saveAllRedactionsTest
+  saveAllRedactionsTest,
+  executeAllRedactScenarios
 } = require("../helpers/mvCaseHelper");
 const {mvData} = require('../pages/common/constants.js');
 
@@ -14,37 +15,37 @@ Feature('Redact Feature');
 Scenario('Mark Content For Redaction Using Draw Box Function ', async ({I}) => {
   await markContentForRedactionUsingDrawBoxTest(I, mvData.REDACTION_CASE, mvData.PDF_DOCUMENT);
 
-}).tag('@ci')
+}).tag('@ignore')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Redact Content Using Redact Text Function', async ({I}) => {
   await redactContentUsingRedactTextTest(I, mvData.REDACTION_CASE, mvData.PDF_DOCUMENT);
 
-}).tag('@ci')
+}).tag('@ignore')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Create Redactions Using Draw Box and Redact Text Functions', async ({I}) => {
   await createRedactionsUsingDrawBoxAndRedactText(I, mvData.REDACTION_CASE, mvData.PDF_DOCUMENT);
 
-}).tag('@ci')
+}).tag('@ignore')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Preview all content marked for redaction', async ({I}) => {
   await previewAllRedactionsTest(I, mvData.REDACTION_CASE, mvData.PDF_DOCUMENT);
 
-}).tag('@ci')
+}).tag('@ignore')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Save redactions to download', async ({I}) => {
   await saveAllRedactionsTest(I, mvData.REDACTION_CASE, mvData.PDF_DOCUMENT);
 
-}).tag('@ci')
+}).tag('@ignore')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Redact text and then removing the redaction', async ({I}) => {
   await redactTextAndThenRemovingRedactionTest(I, mvData.REDACTION_CASE, mvData.PDF_DOCUMENT);
 
-}).tag('@ci')
+}).tag('@ignore')
   .retry(testConfig.TestRetryScenarios);
 
 Scenario('Unmark selected content (marked for redaction)', async ({I}) => {
@@ -58,3 +59,16 @@ Scenario('Unmark all content (marked for redaction)', async ({I}) => {
 
 }).tag('@wip')
   .retry(testConfig.TestRetryScenarios);
+
+Scenario(' Redact Scenarios ==>:: 1.' +
+  '1.Mark Content For Redaction Using Draw Box Function' +
+  '2.Redact Content Using Redact Text Function' +
+  '3. Create Redactions Using Draw Box and Redact Text Functions' +
+  '4. Preview all content marked for redaction' +
+  '5. Save redactions to download' +
+  '6. Redact text and then removing the redaction' +
+  '7.Done', async ({I}) => {
+  await executeAllRedactScenarios(I, mvData.REDACTION_CASE, mvData.PDF_DOCUMENT);
+
+}).tag('@ci')
+  .retry(testConfig.TestRetryScenarios)
