ffde7fc32f4ecc8400adacb43444f30b1f4014a3
phani-perla1 <87771652+phani-perla1@users.noreply.github.com>
2023-01-17 11:47:02 +0000

    EM-4968-MV Dependency Check And ExUi SRT Execution Fix (#661)
    Co-authored-by: PhaniP <phani.perla@hmcts.net>

--- test/end-to-end/pages/annotationsAndComments/collateComments.js
+++ test/end-to-end/pages/annotationsAndComments/collateComments.js
@@ -10,5 +10,5 @@ module.exports = async function () {
   await I.wait(testConfig.BookmarksAndAnnotationsWait);
 
   let commentsList = await I.grabTextFromAll(commonConfig.commentsCount);
-  commentsList.forEach(comment => console.log("Collate Comments==>::" + comment));
+  commentsList.forEach(comment => console.log(comment));
 }
