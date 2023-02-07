5a9b4c99556a8158cecc5d5ada460bf069cdecbb
Jefex7 <duartepireslopes@gmail.com>
2023-02-07 17:36:10 +0000

    Adapted code for safari browser and changed test

--- projects/media-viewer/src/lib/print.service.ts
+++ projects/media-viewer/src/lib/print.service.ts
@@ -12,11 +12,9 @@ export class PrintService {
 
   printElementNatively(element: HTMLElement, width: number, height: number): void {
     const printWindow = window.open('', '', `left=0,top=0,width=${width},height=${height},toolbar=0,scrollbars=0,status=0`);
-    let printContents = element.innerHTML;
-    let printHead = document.head.innerHTML;
-    printWindow.document.write(`<html>
-         ${printHead}
-        <body onload="window.print();">${printContents}</body></html>`);
+    const documentHead = document.head;
+    printWindow.document.body.appendChild(documentHead.cloneNode(true));
+    printWindow.document.body.appendChild(element.cloneNode(true));
     printWindow.document.close();
     printWindow.focus();
     printWindow.print();
