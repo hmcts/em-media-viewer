02a13a506ee7a61f74a57db6e6578c0c1046987e
michaeljacob <Michael.Jacob@HMCTS.NET>
2023-04-17 17:44:51 +0100

    Revert "run tests on aat"
    This reverts commit ecb8e6321f3bc14e7d6a89962676028ad481af26.

--- test/end-to-end/helpers/mvCaseHelper.js
+++ test/end-to-end/helpers/mvCaseHelper.js
@@ -243,14 +243,13 @@ async function previewEnv() {
 }
 
 async function executeTestsOnPreview(I, caseId, mediaType) {
-  // if (process.env.TEST_URL.includes(mvData.PREVIEW_ENV) || process.env.TEST_URL.includes(mvData.LOCAL_ENV)) {
-  //   await I.amOnPage(testConfig.TestUrl, testConfig.PageLoadTime);
-  //   await I.waitForEnabled(commonConfig.assertEnvTestData, testConfig.TestTimeToWaitForText);
-  //   console.log(await I.grabCurrentUrl());
-  // } else {
-    await I.amOnPage('https://manage-case.aat.platform.hmcts.net');
+  if (process.env.TEST_URL.includes(mvData.PREVIEW_ENV) || process.env.TEST_URL.includes(mvData.LOCAL_ENV)) {
+    await I.amOnPage(testConfig.TestUrl, testConfig.PageLoadTime);
+    await I.waitForEnabled(commonConfig.assertEnvTestData, testConfig.TestTimeToWaitForText);
+    console.log(await I.grabCurrentUrl());
+  } else {
     await openCaseDocumentsInMediaViewer(I, caseId, mediaType);
-  // }
+  }
 }
 
 async function uploadDocumentEvent(I, caseId, eventName) {
