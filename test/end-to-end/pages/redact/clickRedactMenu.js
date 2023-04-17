61d4047c667c39d836557c44c0dff081bfc0c022
michaeljacob <Michael.Jacob@HMCTS.NET>
2023-04-17 17:08:59 +0100

    add condition

--- test/end-to-end/pages/redact/clickRedactMenu.js
+++ test/end-to-end/pages/redact/clickRedactMenu.js
@@ -1,9 +1,15 @@
 'use strict'
 const commonConfig = require('../../data/commonConfig.json');
 const testConfig = require("../../../config");
+const {mvData} = require("../common/constants");
 
 module.exports = async function () {
   const I = this;
+  const url = await I.grabCurrentUrl();
+  if (url.includes(mvData.PREVIEW_ENV)) {
+    await I.click(commonConfig.moreOptionsButton)
+    await I.wait(testConfig.BookmarksAndAnnotationsWait);
+  }
   await I.click(commonConfig.redactMenu);
   await I.wait(testConfig.BookmarksAndAnnotationsWait);
 }
