916fce210e17c7161939ad96fb4769eaa4b0dcda
michaeljacob <Michael.Jacob@HMCTS.NET>
2023-04-17 15:23:44 +0100

    prevent preview navigation

--- test/end-to-end/pages/idam/signIn.js
+++ test/end-to-end/pages/idam/signIn.js
@@ -4,11 +4,11 @@ const {mvData} = require("../common/constants");
 
 module.exports = async function () {
   const I = this;
-  if (process.env.TEST_URL.includes(mvData.STAGING_ENV)) {
-    await I.amOnPage(testConfig.TestUrl1, testConfig.PageLoadTime);
-  } else {
-    await I.amOnPage(process.env.TEST_URL, testConfig.PageLoadTime);
-  }
+  // if (process.env.TEST_URL.includes(mvData.STAGING_ENV)) {
+  //   await I.amOnPage(testConfig.TestUrl1, testConfig.PageLoadTime);
+  // } else {
+  //   await I.amOnPage(process.env.TEST_URL, testConfig.PageLoadTime);
+  // }
 
   await I.waitForText('Sign in');
   await I.fillField('username', testConfig.TestEnvCWUser);
