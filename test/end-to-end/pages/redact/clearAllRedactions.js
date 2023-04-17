b8c58a3dc1bc7442ccaa9d5e4449564442724722
michaeljacob <Michael.Jacob@HMCTS.NET>
2023-04-17 12:38:22 +0100

    Fix redaction tests

--- test/end-to-end/pages/redact/clearAllRedactions.js
+++ test/end-to-end/pages/redact/clearAllRedactions.js
@@ -5,8 +5,6 @@ const testConfig = require("../../../config");
 module.exports = async function () {
   const I = this;
 
-  await I.click(commonConfig.moreOptionsButton)
-  await I.wait(testConfig.BookmarksAndAnnotationsWait);
   await I.click(commonConfig.redactMenu);
   await I.wait(testConfig.BookmarksAndAnnotationsWait);
 
