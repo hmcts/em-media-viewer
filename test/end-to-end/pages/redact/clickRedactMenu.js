59343ed63b0161b2313c756cfdf4554c091a8ff2
michaeljacob <Michael.Jacob@HMCTS.NET>
2023-04-18 09:42:13 +0100

    change condition

--- test/end-to-end/pages/redact/clickRedactMenu.js
+++ test/end-to-end/pages/redact/clickRedactMenu.js
@@ -6,7 +6,7 @@ const {mvData} = require("../common/constants");
 module.exports = async function () {
   const I = this;
   const visible = await I.grabNumberOfVisibleElements(commonConfig.redactMenu);
-  if (visible === 0) {
+  if (!visible) {
     await I.click(commonConfig.moreOptionsButton)
     await I.wait(testConfig.BookmarksAndAnnotationsWait);
     await I.click(commonConfig.redactMenuDropdown);
