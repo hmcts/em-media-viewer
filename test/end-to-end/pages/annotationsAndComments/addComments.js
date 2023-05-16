f50e33f713da91f74915d7795cf59f4d5d2c9cf1
MichaelJacob21012 <88201349+MichaelJacob21012@users.noreply.github.com>
2023-05-16 09:13:47 +0100

    highlight all outline on current page (#717)
    * highlight all outline on current page
    
    * add wait

--- test/end-to-end/pages/annotationsAndComments/addComments.js
+++ test/end-to-end/pages/annotationsAndComments/addComments.js
@@ -5,7 +5,8 @@ const testConfig = require("../../../config");
 module.exports = async function () {
   const I = this;
   await I.highlightPdfText();
-  await I.retry(2).click(commonConfig.highLightPopup)
+  await I.retry(2).click(commonConfig.highLightPopup);
+  await I.wait(testConfig.BookmarksAndAnnotationsWait);
   await I.retry(2).click(commonConfig.commentPopup);
   await I.fillField(commonConfig.firstCommentXp, commonConfig.firstComment1);
   await I.retry(3).click(commonConfig.saveButton);
