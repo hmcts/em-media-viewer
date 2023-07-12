a41d06fe7f4a49535dfd6a3ece555adb54cbf81c
michaeljacob <Michael.Jacob@HMCTS.NET>
2023-07-12 12:31:36 +0100

    remove unused code

--- test/end-to-end/pages/annotationsAndComments/deleteAllExistingComments.js
+++ test/end-to-end/pages/annotationsAndComments/deleteAllExistingComments.js
@@ -1,19 +1,11 @@
 'use strict'
 const commonConfig = require('../../data/commonConfig.json');
 const testConfig = require("../../../config");
-let i = 0;
 let commentsList
 
-module.exports = async function (annotationToDelete) {
+module.exports = async function () {
   const I = this;
-  //commentsList = await I.grabTextFromAll(commonConfig.commentsCount);
-
-  // if (commentsList.filter(comment => comment === annotationToDelete)) {
-  //   await I.click(commonConfig.deleteAnnotationText);
-  //   await I.wait(testConfig.BookmarksAndAnnotationsWait);
-  //   await I.click(commonConfig.deleteAnnotationBtn);
-  //   await I.wait(testConfig.BookmarksAndAnnotationsWait);
-  // }
+  let i = 0;
 
   const visible = await I.grabNumberOfVisibleElements(commonConfig.commentsCount)
   console.log(visible);
