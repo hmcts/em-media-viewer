754da166dd56cf2bb92a962e3a48dc50c29d0575
michaeljacob <Michael.Jacob@HMCTS.NET>
2023-07-14 15:51:39 +0100

    waitforelement

--- test/end-to-end/pages/annotationsAndComments/updateComment.js
+++ test/end-to-end/pages/annotationsAndComments/updateComment.js
@@ -8,7 +8,7 @@ module.exports = async function (comment, updatedComment) {
   await I.click(`//p[contains(text(), '${comment}')]`);
   await I.waitForElement(commonConfig.editButton);
   await I.click(commonConfig.editButton);
-  await I.wait(testConfig.BookmarksAndAnnotationsWait);
+  await I.waitForElement(commonConfig.clearFiledXp);
   await I.clearField(commonConfig.clearFiledXp);
   await I.fillField(commonConfig.clearFiledXp, updatedComment);
   await I.click(commonConfig.saveButton);
