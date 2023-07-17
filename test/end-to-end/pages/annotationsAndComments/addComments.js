656a39d2eee9bf63e6c856b9c16aac006b90b4ef
michaeljacob <Michael.Jacob@HMCTS.NET>
2023-07-17 10:29:11 +0100

    waitforelement

--- test/end-to-end/pages/annotationsAndComments/addComments.js
+++ test/end-to-end/pages/annotationsAndComments/addComments.js
@@ -7,6 +7,7 @@ module.exports = async function (commentText) {
   await I.highlightPdfText();
   await I.waitForElement(commonConfig.commentPopup);
   await I.retry(2).click(commonConfig.commentPopup);
+  await I.waitForElement(commonConfig.firstCommentXp);  
   await I.fillField(commonConfig.firstCommentXp, commentText);
   await I.waitForElement(commonConfig.saveButton);
   await I.retry(3).click(commonConfig.saveButton);
