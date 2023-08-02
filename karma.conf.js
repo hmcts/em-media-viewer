78b07cdbbef3655e15abe2a286f06bee2296ff9f
michaeljacob <Michael.Jacob@HMCTS.NET>
2023-08-02 12:53:22 +0100

    update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -3,30 +3,38 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
+  basePath: '',
+  frameworks: ['jasmine', '@angular-devkit/build-angular'],
+  plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
+  ],
+  client: {
+      jasmine: {
+      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
+  },
+  jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+  },
+  coverageReporter: {
       dir: require('path').join(__dirname, '../../coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+      subdir: '.',
+      reporters: [
+      { type: 'html', subdir: 'html-report' },
+      { type: 'lcov', subdir: 'lcov-report' }
+      ]
+  },
+  reporters: ['progress', 'kjhtml'],
+  port: 9876,
+  colors: true,
+  logLevel: config.LOG_INFO,
+  autoWatch: true,
+  browsers: ['Chrome'],
+  singleRun: false,
+  restartOnFileChange: true
   });
 };
