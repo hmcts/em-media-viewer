a0edc8c538d93aaea489872c7cea80073e745d58
Raihan Rahman <134499230+Raihan-R@users.noreply.github.com>
2023-09-11 14:09:57 +0100

    em-4957: testing core functionality - smoke test

--- test/end-to-end/mvFeatures/smokeTest.js
+++ test/end-to-end/mvFeatures/smokeTest.js
@@ -1,10 +1,21 @@
 const testConfig = require('../../config');
-const {loginTest} = require("../helpers/mvCaseHelper");
+const {loginTest, updateBookmarkTest, addCommentTest, printDocumentFromMVTest, markContentForRedactionUsingDrawBoxTest, commentsSearchTest} = require("../helpers/mvCaseHelper");
+const {mvData} = require('../pages/common/constants.js');
 
-Feature('ExUI login Smoke Test');
+Feature('Smoke Test');
 
 Scenario('Login to the manage case application', async ({I}) => {
   await loginTest(I);
+
+}).retry(testConfig.TestRetryScenarios)
+  .tag('@wip')
+
+Scenario('Core Functionality Tested', async ({I}) => {
+  await updateBookmarkTest(I, mvData.CASE_ID, mvData.PDF_DOCUMENT);
+  await addCommentTest(I, mvData.CASE_ID, mvData.PDF_DOCUMENT);
+  await commentsSearchTest(I, mvData.CASE_ID, mvData.PDF_DOCUMENT);
+  await printDocumentFromMVTest(I, mvData.CASE_ID, mvData.PDF_DOCUMENT);
+  await markContentForRedactionUsingDrawBoxTest(I, mvData.CASE_ID, mvData.PDF_DOCUMENT);
 
 }).retry(testConfig.TestRetryScenarios)
   .tag('@wip')
