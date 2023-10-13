9dad5275bb646315304cb8352af5c60628205227
michaeljacob <Michael.Jacob@HMCTS.NET>
2023-10-13 09:32:46 +0100

    fix image viewer tests

--- test/end-to-end/pages/steps.js
+++ test/end-to-end/pages/steps.js
@@ -69,6 +69,7 @@ module.exports = () => {
     clickRedactAllButton: steps.redact.clickRedactAllButton,
     hrsSignIn: steps.hrsPlatform.hrsSignIn,
     findCase: steps.hrsPlatform.findCase,
-    shareHRSFile: steps.hrsPlatform.shareHRSFile
+    shareHRSFile: steps.hrsPlatform.shareHRSFile,
+    openImage: steps.imageViewer.openImage
   });
 };
