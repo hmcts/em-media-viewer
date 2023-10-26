d0e9c3de95a664f2d5974547532d5facd2037041
Raihan Rahman <134499230+Raihan-R@users.noreply.github.com>
2023-10-26 09:49:03 +0100

    add reorderBookmarks to steps

--- test/end-to-end/pages/steps.js
+++ test/end-to-end/pages/steps.js
@@ -28,6 +28,7 @@ module.exports = () => {
     sortBookmarks: steps.bookMarks.sortBookmarks,
     addEmptyBookmarks: steps.bookMarks.addAnEmptyBookMarks,
     customOrderBookmarks: steps.bookMarks.customOrderBookmarks,
+    reorderBookmarks: steps.bookMarks.reorderBookmarks,
     mvAudioScenario: steps.multiMedia.multiMediaAudio,
     highlightPdfText: steps.annotationsAndComments.highlightPdfText,
     addCommentAndRotate: steps.annotationsAndComments.addCommentAndRotate,
