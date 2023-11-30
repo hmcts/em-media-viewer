bafee0b7df81b8b11ee1ec501b9375d0d0c922c4
Munish Sharma <Munish.Sharma@HMCTS.NET>
2023-11-30 10:13:37 +0000

    EM-5673 : failing tests

--- test/end-to-end/pages/printAndDownload/mvDownload.js
+++ test/end-to-end/pages/printAndDownload/mvDownload.js
@@ -3,8 +3,7 @@ const commonConfig = require('../../data/commonConfig.json');
 
 module.exports = async function () {
   const I = this;
-  await I.retry(2).click(commonConfig.moreOptionsButton);
-  await I.retry(3).click('.dropdown-menu #mvDownloadBtn');
+  await I.retry(3).click('#mvDownloadBtn');
   const screenshotName = Date.now() + 'download' + '.png';
   await I.saveScreenshot(screenshotName, true);
 };
