8416cdfc3812c40757e29e77c25908679f4136d2
Munish Sharma <Munish.Sharma@HMCTS.NET>
2023-11-30 12:06:18 +0000

    using xpath

--- test/end-to-end/pages/annotationsAndComments/clickCommentsPanel.js
+++ test/end-to-end/pages/annotationsAndComments/clickCommentsPanel.js
@@ -4,8 +4,7 @@ const testConfig = require("../../../config");
 
 module.exports = async function () {
   const I = this;
-  await I.click(commonConfig.moreOptionsButton)
   await I.wait(testConfig.BookmarksAndAnnotationsWait);
-  await I.click(commonConfig.commentsBtn);
+  await I.click(commonConfig.commentsBtnId);
   await I.wait(testConfig.BookmarksAndAnnotationsWait);
 }
