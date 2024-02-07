484470a44b1f768be66fd91fce5fec3ee1b983f9
Raihan Rahman <134499230+Raihan-R@users.noreply.github.com>
2024-02-07 11:46:05 +0000

    em-5816: fix to Redaction test (#923)

--- test/end-to-end/pages/redact/clickRedactAllButton.js
+++ test/end-to-end/pages/redact/clickRedactAllButton.js
@@ -1,18 +1,18 @@
 'use strict'
 const commonConfig = require('../../data/commonConfig.json');
 const testConfig = require("../../../config");
-const { assert } = require('chai')
 
 module.exports = async function () {
   const I = this;
   const countText = await I.retry(10).grabTextFrom(commonConfig.findRedactResultsCount);
+  const countValueString = countText.replace('results founds', '')
+  const countValue = Number(countValueString.trim());
+
   await I.checkElementExist(commonConfig.redactAllBtn)
   await I.click(commonConfig.redactAllBtn)
   await I.wait(testConfig.BookmarksAndAnnotationsWait);
-  const countValueString = countText.replace('results founds', '')
-  const countValue = Number(countValueString.trim());
-  const retangleFound = await I.retry(10).grabHTMLFromAll(commonConfig.rectangleClass);
-  assert.equal(retangleFound.length, countValue)
+
+  await I.retry(3).seeNumberOfElements(commonConfig.rectangleClass, countValue);
 }
 
 
