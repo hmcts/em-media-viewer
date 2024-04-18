ddd087e4dc7db35717aa825fa6eb791f8bf30f87
michaeljacob <Michael.Jacob@HMCTS.NET>
2024-04-18 12:35:45 +0100

    lint

--- projects/media-viewer/src/lib/annotations/annotation-set/annotation-view/rectangle/resizable-element.directive.ts
+++ projects/media-viewer/src/lib/annotations/annotation-set/annotation-view/rectangle/resizable-element.directive.ts
@@ -14,7 +14,7 @@ import {
 import { ResizeHandlersComponent } from './resize-handlers/resize-handlers.component';
 
 @Directive({
-  selector: '[resizable]'
+  selector: '[mv-resizable]'
 })
 export class ResizableElementDirective implements OnInit, OnChanges {
 
