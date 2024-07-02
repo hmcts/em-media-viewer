e95e2c90f5a4b4648e4ce7d4cd8530cf29ce7a20
michaeljacob <Michael.Jacob@HMCTS.NET>
2024-07-02 11:38:10 +0100

    use assert

--- test/end-to-end/pages/annotationsAndComments/deleteAllExistingTextHighlights.js
+++ test/end-to-end/pages/annotationsAndComments/deleteAllExistingTextHighlights.js
@@ -2,18 +2,20 @@
 
 const commonConfig = require('../../data/commonConfig.json');
 const testConfig = require("../../../config");
+const {assert} = require('chai')
 
 module.exports = async function () {
   const I = this;
   
   let i = 0;
 
-  const visible = await I.grabNumberOfVisibleElements(commonConfig.highLightTextCount);
-  while (i < visible) {
+  const initial = await I.grabNumberOfVisibleElements(commonConfig.highLightTextCount);
+  while (i < initial) {
     await I.click(commonConfig.highLightTextCount);
     await I.waitForElement(commonConfig.commentPopup.replace('Comment', 'Delete'));
     await I.click(commonConfig.commentPopup.replace('Comment', 'Delete'));
     ++i;
-    await I.waitNumberOfVisibleElements(commonConfig.highLightTextCount, visible - i);
+    const remaining = await I.grabNumberOfVisibleElements(commonConfig.highLightTextCount);
+    assert.equal(remaining, initial - i);
   }
 }
