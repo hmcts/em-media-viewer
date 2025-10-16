054552a315bd6e197f06e2ddb54f8d2ba7e8210c
Chris Jones <christopher.jones@hmcts.net>
2025-10-16 17:06:24 +0100

    fix for indentation

--- projects/media-viewer/src/lib/shared/directives/tooltip-dismiss.directive.ts
+++ projects/media-viewer/src/lib/shared/directives/tooltip-dismiss.directive.ts
@@ -4,20 +4,20 @@ import { Directive, ElementRef, HostListener } from "@angular/core";
   selector: '.tooltip, [mvTooltipDismiss]'
 })
 export class TooltipDismissDirective {
-    constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef) {}
 
-    @HostListener('document:keydown.escape', ['$event'])
-    onEscapeDismissTooltip() {
-        const element = this.el.nativeElement as HTMLElement;
-        element.setAttribute('data-tooltip-dismissed', 'true');
-    }
+  @HostListener('document:keydown.escape', ['$event'])
+  onEscapeDismissTooltip() {
+    const element = this.el.nativeElement as HTMLElement;
+    element.setAttribute('data-tooltip-dismissed', 'true');
+  }
 
-    @HostListener('mouseenter')
-    @HostListener('focus')
-    @HostListener('focusin')
-    onShowTooltip() {
-        const element = this.el.nativeElement as HTMLElement;
-        element.removeAttribute('data-tooltip-dismissed');
-    }
+  @HostListener('mouseenter')
+  @HostListener('focus')
+  @HostListener('focusin')
+  onShowTooltip() {
+    const element = this.el.nativeElement as HTMLElement;
+    element.removeAttribute('data-tooltip-dismissed');
+  }
 
 }
