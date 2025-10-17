f35a029694c4e7e582a29d5fab924762e6739bc0
Chris Jones <christopher.jones@hmcts.net>
2025-10-17 09:14:21 +0100

    renaming new tooltip class

--- projects/media-viewer/src/lib/shared/directives/tooltip-dismiss.directive.ts
+++ projects/media-viewer/src/lib/shared/directives/tooltip-dismiss.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef, HostListener } from "@angular/core";
 
 @Directive({
-  selector: '.tooltip, [mvTooltipDismiss]'
+  selector: '.mv-tooltip, [mvTooltipDismiss]'
 })
 export class TooltipDismissDirective {
   constructor(private el: ElementRef) {}
