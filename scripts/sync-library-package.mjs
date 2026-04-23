fe8880f8c77bb5da20932b34ae673b869f302f88
Olu <142989683+olusegz07@users.noreply.github.com>
2026-04-23 16:01:40 +0100

    fix: publish media-viewer with library deps only

--- scripts/sync-library-package.mjs
+++ scripts/sync-library-package.mjs
@@ -6,36 +6,23 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const repoRoot = path.resolve(__dirname, '..');
-const rootPackagePath = path.join(repoRoot, 'package.json');
 const libraryPackagePath = path.join(repoRoot, 'projects', 'media-viewer', 'package.json');
 const distPackagePath = path.join(repoRoot, 'dist', 'media-viewer', 'package.json');
 
-const rootPackage = readJson(rootPackagePath);
 const libraryPackage = readJson(libraryPackagePath);
 const distPackage = readJson(distPackagePath);
 
-const libraryPeerDependencies = libraryPackage.peerDependencies || {};
-const distDependencies = distPackage.dependencies || {};
-const rootDependencies = rootPackage.dependencies || {};
+distPackage.dependencies = sortObject({
+  ...(distPackage.dependencies || {}),
+  ...(libraryPackage.dependencies || {}),
+});
 
-const mergedDependencies = { ...distDependencies };
+distPackage.peerDependencies = sortObject({
+  ...(distPackage.peerDependencies || {}),
+  ...(libraryPackage.peerDependencies || {}),
+});
 
-for (const [dependencyName, version] of Object.entries(rootDependencies)) {
-  if (libraryPeerDependencies[dependencyName]) {
-    continue;
-  }
-
-  if (!mergedDependencies[dependencyName]) {
-    mergedDependencies[dependencyName] = version;
-  }
-}
-
-distPackage.dependencies = Object.keys(mergedDependencies)
-  .sort()
-  .reduce((dependencies, dependencyName) => {
-    dependencies[dependencyName] = mergedDependencies[dependencyName];
-    return dependencies;
-  }, {});
+delete distPackage.devDependencies;
 
 writeJson(distPackagePath, distPackage);
 
@@ -45,4 +32,13 @@ function readJson(filePath) {
 
 function writeJson(filePath, value) {
   writeFileSync(filePath, `${JSON.stringify(value, null, 2)}\n`);
+}
+
+function sortObject(value) {
+  return Object.keys(value)
+    .sort()
+    .reduce((result, key) => {
+      result[key] = value[key];
+      return result;
+    }, {});
 }
