029ccba761d32e62f77240d8bd87eb39f75b6eda
Andrew Grizhenkov <andrew.grizhenkov@hmcts.net>
2026-06-23 13:53:07 +0100

    fix: use puppeteer chrome for karma tests

--- projects/media-viewer/karma.conf.js
+++ projects/media-viewer/karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function (config) {
     config.set({
       basePath: '',
@@ -38,7 +40,7 @@ module.exports = function (config) {
       colors: true,
       logLevel: config.LOG_INFO,
       autoWatch: true,
-      browsers: ['ChromeHeadless'],
+      browsers: ['ChromeHeadlessCI'],
       customLaunchers: {
         ChromeHeadlessCI: {
           base: 'ChromeHeadless',
@@ -49,4 +51,3 @@ module.exports = function (config) {
       restartOnFileChange: true
     });
   };
-  
